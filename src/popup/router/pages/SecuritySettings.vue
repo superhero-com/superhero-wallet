f1837401658dc0d4e2863814609f3fe1aa97fc2b
Milen Radkov <milenradkov@me.com>
2020-01-31 20:31:55 +0200

    fix problem with seed backup and updated UI

--- src/popup/router/pages/SecuritySettings.vue
+++ src/popup/router/pages/SecuritySettings.vue
@@ -1,19 +1,21 @@
 <template>
     <div class="popup">
-        <div class="actions">
-            <button class="backbutton toAccount" @click="navigateToSettings"><ae-icon name="back" /> {{$t('pages.securitySettings.backToSettings')}}</button>
-        </div>
-        <h3 style='text-align:center;'>{{$t('pages.securitySettings.heading')}}</h3>
-        <ae-panel>
-            <div class="maindiv_input-group-addon">
-                <h4>{{$t('pages.securitySettings.seedRecoveryHeading')}}</h4><hr>
-                <small class="sett_info">{{$t('pages.securitySettings.seedRecoverySmall')}}</small>
-                <ae-button face="round" fill="primary" class="notround settingBtn" extend @click="seedPhraseRecovery">{{$t('pages.securitySettings.seedRecoveryBtn')}}</ae-button>
+        <BackLink to="/account">
+            {{ $t('pages.securitySettings.backToSettings') }}
+        </BackLink>
+        <div v-if="type == ''">
+            <h3 style='text-align:center;'>{{$t('pages.securitySettings.heading')}}</h3>
+            <ae-panel>
+                <div class="maindiv_input-group-addon">
+                    <h4>{{$t('pages.securitySettings.seedRecoveryHeading')}}</h4><hr>
+                    <small class="sett_info">{{$t('pages.securitySettings.seedRecoverySmall')}}</small>
+                    <ae-button face="round" fill="primary" class="notround settingBtn" extend @click="seedPhraseRecovery">{{$t('pages.securitySettings.seedRecoveryBtn')}}</ae-button>
+                </div>
+            </ae-panel>
+            <popup :popupSecondBtnClick="popup.secondBtnClick"></popup>
+            <div v-if="loading" class="loading">
+                <ae-loader />
             </div>
-        </ae-panel>
-        <popup :popupSecondBtnClick="popup.secondBtnClick"></popup>
-        <div v-if="loading" class="loading">
-            <ae-loader />
         </div>
         <Modal v-if="type == 3" :modal="modal">
             <div slot="content">
@@ -24,22 +26,45 @@
                         {{alert.content}}
                     </div>
                 </Alert>
-                <ae-toolbar fill="alternative" v-if="seedPhrase != ''" align="right">
-                    <ae-button face="toolbar" v-clipboard:copy="seedPhrase" @click="reset(seedPhrase)">
+                <ae-panel style="box-shadow: #c6c3c3 0px 0px 20px;margin:0">
+                    <p style="word-spacing: 10px;">{{ seedPhrase }}</p>
+                    <ae-button style="float: right;margin: 10px 0 30px 0;" face="toolbar" v-clipboard:copy="seedPhrase">
                         <ae-icon name="copy" />
                         {{$t('pages.securitySettings.copy')}}
                     </ae-button>
-                </ae-toolbar>
-                <div v-if="seedPhrase == '' && !loading">
-                    <ae-input class="my-2" label="Password">
-                        <input type="password" class="ae-input"  placeholder="Enter password" v-model="password" slot-scope="{ context }" @focus="context.focus = true" @blur="context.focus = false" />
-                    </ae-input>
-                    <ae-button class="notround decrypt-btn" extend face="round" fill="primary" @click="decryptKeystore">{{$t('pages.securitySettings.showSeedPhrase')}}</ae-button>
-                </div>
-                <Loader :loading="loading" size="small" :content="$t('pages.securitySettings.decryptingPrivateKey')"></Loader>
+                </ae-panel>
+                <span>
+                    Back up your seed phrase on a piece of paper or file that no one except you will ever see. Make multiple copies.
+                </span>
+                <p>Do not lose this seed phrase!</p>
+                <small>To make sure you got it right, you will be asked to recreate this seed phrase in the next screen.</small>
+                <button @click="verifySeed" class="primary-button">Verify seed</button>
+                <button @click="navigateToAccount" class="primary-button">I've already done this</button>
                 <popup :popupSecondBtnClick="popup.secondBtnClick"></popup>
             </div>
         </Modal>
+        <div v-if="type == 4">
+            <h3  class="phraseTitle">{{$t('pages.seedPhrase.confirmSeedPhrase') }}</h3>
+            <ae-phraser>
+                <ae-badge class="seedBadge" :class="{'selected':seed.selected}" v-for="(seed,index) in seeds" v-bind:key="seed.id" @click.native="selectSeed(seed.name,index,seed.id)">{{seed.name}}</ae-badge>
+            </ae-phraser>
+            <div class="phraseSubTitle">{{$t('pages.seedPhrase.recoveryPhrase') }}</div>
+            <ae-phraser v-if="selectedSeed.length == 0">
+                <ae-badge class="seedBadge selected">{{$t('pages.seedPhrase.first') }}</ae-badge>
+                <ae-badge class="seedBadge selected">{{$t('pages.seedPhrase.second') }}</ae-badge>
+                <ae-badge class="seedBadge selected">{{$t('pages.seedPhrase.third') }}</ae-badge>
+                <ae-badge class="seedBadge selected">...</ae-badge>
+            </ae-phraser>
+            <ae-phraser v-bind="seedError">
+                <ae-badge class="seedBadge" v-for="(seed,index) in selectedSeed" v-bind:key="seed.id" @click.native="removeSeed(seed.parent,index)">{{seed.name}} <ae-icon name="close" class="seedClose" /></ae-badge>
+            </ae-phraser>
+            <button @click="verifyLastStep" class="primary-button" style="width:50%">Verify</button>
+        </div>
+        <div v-if="seed_verified && type == 5">
+            <ae-icon style="color:#e911ff; font-size:100px;" name="check" />
+            <p>Seed phrase confirmed.</p>
+            <button @click="navigateToAccount" class="primary-button">Back to Dashboard</button>
+        </div>
     </div>
 </template>
 
@@ -48,26 +73,40 @@ import { mapGetters } from 'vuex';
 import { getHdWalletAccount } from '../../utils/hdWallet';
 import { decrypt } from '../../utils/keystore';
 import { addressGenerator, decryptMnemonic } from '../../utils/address-generator';
+import {shuffleArray} from '../../utils/helper';
 
 export default {
     data () {
         return {
             loading: false,
-            onoff: false,
             modal:{
                 visible:false,
                 title:''
             },
-            password:'',
-            privateKey:'',
             seedPhrase: '',
             alert: {
                 fill:"neutral",
                 show:false,
                 content:""
             },
-            loading:false,
-            type: ''
+            type: '',
+            seeds: [
+                {id:0,name:"volcano",selected:false},
+                {id:1,name:"entire",selected:false},
+                {id:2,name:"magnet",selected:false},
+                {id:3,name:"glow",selected:false},
+                {id:4,name:"zero",selected:false},
+                {id:5,name:"crack",selected:false},
+                {id:6,name:"arena",selected:false},
+                {id:7,name:"episode",selected:false},
+                {id:8,name:"shrimp",selected:false},
+                {id:9,name:"buffalo",selected:false},
+                {id:10,name:"tiny",selected:false},
+                {id:11,name:"aunt",selected:false}
+            ],
+            selectedSeed:[],
+            seedError:{},
+            seed_verified: false
         }
     },
     computed: {
@@ -76,80 +115,71 @@ export default {
     created() {
     },
     methods: {
-        navigateToSettings() {
-            this.$router.push('/settings')
-        },
-        clearPrivacyConfirm() {
-            this.$store.dispatch('popupAlert', {
-                name: 'account',
-                type: 'confirm_privacy_clear'
-            });
-        },
-        clearPrivacyData( ) {
-            this.type = '1';
-            browser.storage.local.remove('connectedAepps')
-        },
         seedPhraseRecovery() {
             this.type = '3';
             this.modal.visible = true
             this.modal.title = this.$t('pages.securitySettings.showSeedPhrase')
-            this.reset()
+            this.loading = true
+            browser.storage.local.get('mnemonic').then(async (seed) => {
+                this.seedPhrase = seed.mnemonic
+                this.setAlertData("alternative",true,seed.mnemonic)
+                var seedPhraseToArray = seed.mnemonic.split(" ");
+                this.seeds.forEach(function(item, index) {
+                    item.name = seedPhraseToArray[index]
+                });
+            });
         },
-        decryptKeystore() {
-            if (this.type == '3') {
-                this.loading = true
-                browser.storage.local.get('userAccount').then(async (user) => {
-                    if(user.userAccount && user.hasOwnProperty('userAccount')) {
-                        let encryptedPrivateKey = JSON.parse(user.userAccount.encryptedPrivateKey);
-                        let match = await addressGenerator.decryptKeystore(encryptedPrivateKey, this.password)
-                        this.loading = false
-                        if(match) {
-                            browser.storage.local.get('encryptedSeed').then(async (res) => {
-                                if (res.encryptedSeed && res.hasOwnProperty('encryptedSeed')) {
-                                    // let decryptedSeed = cryptr.decrypt(res.encryptedSeed);
-                                    let decryptedSeed = await decryptMnemonic(res.encryptedSeed, this.password);
-                                    this.seedPhrase = decryptedSeed;
-                                    this.setAlertData("alternative",true,decryptedSeed)
-                                } else {
-                                    this.$store.dispatch('popupAlert', { name: 'account', type: 'reveal_seed_phrase_impossible'});
-                                }
-                            });
-                        }else {
-                            this.setAlertData("primary",true,this.$t('pages.securitySettings.incorrectPassword'))
-                        }
-                    }
-                })
-            } 
+        navigateToAccount() {
+            this.$router.push('/account')
         },
-        reset(privateKey = '') {
-            if(privateKey == '') {
-                this.resetFields()
-            }else {
-                this.$copyText(privateKey).then(e => {
-                    this.resetFields()
-                })
-            }
-        },
-        reset(seedPhrase = '') {
-            if(seedPhrase == '') {
-                this.resetFields()
-            }else {
-                this.$copyText(seedPhrase).then(e => {
-                    this.resetFields()
-                })
-            }
-        },
-        resetFields() {
-            this.privateKey = ''
-            this.seedPhrase = ''
-            this.alert.show = false
-            this.alert.content = ''
-            this.password = ''
+        verifySeed() {
+            this.type = '4';
+            shuffleArray(this.seeds)
         },
         setAlertData(fill,show,content) {
             this.alert.fill = fill
             this.alert.show = show
             this.alert.content = content
+        },
+        selectSeed(seed,index,id) {
+            if(!this.selectedSeed.find(s => {return s.parent == id })) {
+                this.selectedSeed.push({name:seed,parent:id});
+                this.seeds.find(s => s.id == id).selected = true;
+            }
+            if(this.selectedSeed.length == 12) {
+                this.buttonFill = "primary";
+            }else {
+                this.buttonFill = "";
+            }
+        },
+        removeSeed(parent,index) {
+            this.seeds.find(s => s.id == parent).selected = false;
+            this.selectedSeed.splice(index,1);
+            if(this.selectedSeed.length == 12) {
+                this.buttonFill = "primary";
+            }else {
+                this.buttonFill = "";
+            }
+            this.seedError = {};
+        },
+        verifyLastStep() {
+            let seed = this.seeds.slice();
+            let sorted = seed.sort((a, b) => (a.id > b.id) ? 1 : -1);
+            let originalSeed = sorted.map(seed => seed.name).join(",");
+            const selectSeed = this.selectedSeed.map(seed => seed.name).join(",");
+            if(this.selectedSeed.length == 12) {
+                if(originalSeed != selectSeed) {
+                    this.seedError = {"error":"Oops! Incorrect seed phrase!"}
+                }else {
+                    this.seedError = {};
+                    this.loading = true;
+                    this.seed_verified = true;
+                    this.type = '5';
+                    browser.storage.local.set({backed_up_Seed: true}).then(() => {});
+                }
+            } else {
+                this.seedError = {"error":"Oops! Incorrect length of words!"}
+            }
         }
     }
 }
@@ -158,6 +188,14 @@ export default {
 <style lang="scss">
 
 @import '../../../common/base';
+.primary-button {
+    background: #4f4f4f;
+    color: #fff;
+    margin: 1rem auto 0;
+    padding: 1rem;
+    font-weight: bold;
+    border-radius: 10px;
+}
 .regbtn{
     background: #FF0D6A;
     color: #ffffff;
@@ -199,5 +237,19 @@ input:active,input:focus {
 .notround { border-radius: 0 !important; }
 small {
     word-break: break-word;
+}
+.seedBadge { 
+    user-select: unset;
+    cursor:pointer;
+    border: 2px solid #EDF3F7;
+    .seedClose {
+        margin-left:5px;
+    }
+    &.selected { 
+        opacity:0.4;
+        cursor: unset;
+        background: transparent;
+        border: 2px solid #c1c1c1;
+    }
 }
 </style>
