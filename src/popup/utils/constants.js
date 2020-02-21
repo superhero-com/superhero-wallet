49c1c148f99722e80ccca3af7333c108496b5ec7
Petar Baykov <petbaik26@gmail.com>
2020-02-21 11:09:01 +0200

    Update endpoint for checking current tipping contract version
    Fixes: 88

--- src/popup/utils/constants.js
+++ src/popup/utils/constants.js
@@ -115,6 +115,7 @@ export const networks = {
   }
 }
 export const TIP_SERVICE = 'https://payfortx.z52da5wt.xyz';
+export const BACKEND_URL = 'https://backend.z52da5wt.xyz';
 
 export const TX_LIMIT_PER_DAY = 2000
 export const TIPPING_CONTRACT =
