a6b54b3325f7eeb5ccf2e5ecc38ba28403250f9e
Petar Baykov <petbaik26@gmail.com>
2020-02-28 13:54:57 +0200

    Change Corona with SuperHero

--- src/phishing/App.vue
+++ src/phishing/App.vue
@@ -2,16 +2,16 @@
   <div class="content">
     <ae-main>
       <ae-panel class="text-center">
-        <img :src="logo" alt="Corona logo" />
-        <h1><ae-icon fill="primary" face="round" name="info" />Corona Phishing Detection</h1>
+        <img :src="logo" alt="SuperHero logo" />
+        <h1><ae-icon fill="primary" face="round" name="info" />SuperHero Phishing Detection</h1>
         <p>
-          This domain is currently on the Corona domain warning list. This means that based on information available to us, Corona believes this domain could currently compromise
-          your security and, as an added safety feature, Corona has restricted access to the site. To override this, please read the rest of this warning for instructions on how to
+          This domain is currently on the SuperHero domain warning list. This means that based on information available to us, SuperHero believes this domain could currently compromise
+          your security and, as an added safety feature, SuperHero has restricted access to the site. To override this, please read the rest of this warning for instructions on how to
           continue at your own risk.
         </p>
         <p>
           There are many reasons sites can appear on our warning list, and our warning list compiles from other widely used industry lists. Such reasons can include known fraud or
-          security risks, such as domains that test positive on the <a>Corona Phishing Detector</a>. Domains on these warning lists may include outright malicious websites and
+          security risks, such as domains that test positive on the <a>SuperHero Phishing Detector</a>. Domains on these warning lists may include outright malicious websites and
           legitimate websites that have been compromised by a malicious actor.
         </p>
         <p>To read more about this site <a>please review the domain on </a>.</p>
