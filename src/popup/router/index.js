0c7def43e2085d1fe9c558ff73fbb0b299e5082d
Denis Davidyuk <denis_davidyuk@hotmail.com>
2020-03-06 15:36:27 +1000

    Setup deeplinks

--- src/popup/router/index.js
+++ src/popup/router/index.js
@@ -83,20 +83,26 @@ router.afterEach(to => {
   localStorage.setItem(lastRouteKey, to.path);
 });
 
-window.handleOpenURL = async url => {
-  if (!store.getters.isLoggedIn) {
-    await Promise(resolve => {
-      const unsubscribe = store.watch(
-        (state, { isLoggedIn }) => isLoggedIn,
-        isLoggedIn => {
-          if (!isLoggedIn) return;
-          unsubscribe();
-          resolve();
-        }
-      );
+if (!process.env.IS_EXTENSION) {
+  document.addEventListener('deviceready', () => {
+    window.IonicDeeplink.onDeepLink(async ({ url }) => {
+      if (!store.getters.isLoggedIn) {
+        await Promise(resolve => {
+          const unsubscribe = store.watch(
+            (state, { isLoggedIn }) => isLoggedIn,
+            isLoggedIn => {
+              if (!isLoggedIn) return;
+              unsubscribe();
+              resolve();
+            }
+          );
+        });
+      }
+      const prefix = ['superhero:', 'https://mobile.z52da5wt.xyz/'].find(p => url.startsWith(p));
+      if (!prefix) throw new Error(`Unknown url: ${url}`);
+      router.push(`/${url.slice(prefix.length)}`);
     });
-  }
-  router.push(`/${url.replace(/^superhero:/, '')}`);
-};
+  });
+}
 
 export default router;
