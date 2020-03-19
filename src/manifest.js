c4a0cb4803ea3c9896fb5599e36625f60fe013a5
Denis Davidyuk <denis_davidyuk@hotmail.com>
2020-03-19 14:59:13 +1000

    Combine manifests files

---
+++ src/manifest.js
@@ -0,0 +1,52 @@
+module.exports = (isProd, browser) => ({
+  name: 'SuperHero',
+  description: 'SuperHero Wallet',
+  version: process.env.npm_package_version,
+  manifest_version: 2,
+  ...(browser === 'firefox' && {
+    applications: {
+      gecko: {
+        strict_min_version: '53.0',
+      },
+    },
+  }),
+  permissions: [
+    'storage',
+    'unlimitedStorage',
+    'videoCapture',
+    'activeTab',
+    'clipboardWrite',
+    'notifications',
+    'tabs',
+    'webRequest',
+    'webRequestBlocking',
+    '*://*.chain/*',
+    '*://*.google.com/*',
+  ],
+  icons: {
+    48: './icons/icon_48.png',
+    128: './icons/icon_128.png',
+  },
+  content_security_policy: `script-src 'self'${isProd ? '' : " 'unsafe-eval'"}; object-src 'self'`,
+  browser_action: {
+    default_title: 'SuperHero',
+    default_popup: 'popup/popup.html',
+  },
+  background: {
+    scripts: ['background.js'],
+    persistent: true,
+  },
+  options_ui: {
+    page: 'options/options.html',
+    chrome_style: true,
+  },
+  content_scripts: [
+    {
+      run_at: 'document_start',
+      all_frames: !isProd && browser === 'chrome',
+      matches: ['https://*/*', 'http://*/*'],
+      js: ['inject.js'],
+    },
+  ],
+  web_accessible_resources: ['inject.js', 'phishing/phishing.html', 'phishing/phishing.js', 'popup/CameraRequestPermission.html'],
+});
