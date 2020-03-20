4be27145a24f34f248c0324463a6074babd6f65f
Valentin Angelov <43376240+vmangelovv@users.noreply.github.com>
2020-03-20 21:24:25 +0200

    Show user's avatar instead of identicon, fixes #34

---
+++ src/popup/router/components/UserAvatar.vue
@@ -0,0 +1,31 @@
+<template>
+  <img :src="avatarUrl" class="user-avatar" :class="size" />
+</template>
+
+<script>
+import { BACKEND_URL } from '../../utils/constants';
+
+export default {
+  props: {
+    address: String,
+    size: String,
+  },
+  computed: {
+    avatarUrl() {
+      return `${BACKEND_URL}/profile/image/${this.address}`;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.user-avatar {
+  width: 38px;
+  height: 38px;
+  border-radius: 50%;
+}
+.user-avatar.lg {
+  height: 4rem;
+  width: 4rem;
+}
+</style>
