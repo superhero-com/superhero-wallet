cc3cb2b50297ff4a9812d997c1d00452d933ebd3
Petar Baykov <petbaik26@gmail.com>
2020-03-20 19:17:38 +0200

    Fix space that cause fail of CI

--- src/store/index.js
+++ src/store/index.js
@@ -34,7 +34,7 @@ export default new Vuex.Store({
       pending: [],
     },
     sdk: null,
-    middleware:null,
+    middleware: null,
     aeppPopup: false,
     ledgerApi: null,
     txAdvancedMode: false,
