db95ae72d67137147a6a837c69886b59c8432dde
mradkov <milenradkov@me.com>
2020-03-21 07:28:51 +0200

    add notification route-link

--- src/popup/router/pages/Notifications.vue
+++ src/popup/router/pages/Notifications.vue
@@ -3,7 +3,9 @@
     <ae-list class="noti-list">
       <ae-list-item fill="neutral" v-for="(noti, i) in notifications" :key="i" class="noti">
         <img src="../../../icons/icon_48.png" />
-        <span> {{ noti.content }} </span>
+        <span>
+          <router-link :to="noti.route"> {{ noti.content }} </router-link>
+        </span>
       </ae-list-item>
     </ae-list>
   </div>
