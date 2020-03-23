d19039675fd0a5aa181606d2d8be3cf89e1b19fc
Denis Davidyuk <denis_davidyuk@hotmail.com>
2020-03-23 14:37:49 +1000

    Avoid cycle imports

--- src/popup/router/components/PendingTxs.vue
+++ src/popup/router/components/PendingTxs.vue
@@ -26,21 +26,28 @@ export default {
   components: {
     Eye,
   },
-  computed: {
-    ...mapGetters(['transactions', 'currentCurrency']),
-  },
+  computed: mapGetters(['transactions', 'currentCurrency']),
   filters: { formatDate },
-  data() {
-    return {
-      checkMined: null,
-    };
-  },
   created() {
     this.$store.dispatch('getPendingTxs');
-    this.checkMined = setInterval(() => this.$store.dispatch('checkPendingTxMined'), 2500);
+    const checkMined = setInterval(() => this.checkPendingTxMined(), 2500);
+    this.$once('hook:destroyed', () => clearInterval(checkMined));
   },
-  beforeDestroy() {
-    clearInterval(this.checkMined);
+  methods: {
+    async checkPendingTxMined() {
+      const { pendingTxs = [] } = await browser.storage.local.get('pendingTxs');
+      await Promise.all(
+        pendingTxs.map(async ({ hash, type, amount, domain }) => {
+          const mined = await this.$store.state.sdk.poll(hash);
+          if (!mined) return;
+          const pending = pendingTxs.filter(p => p.hash !== hash);
+          await browser.storage.local.set({ pendingTxs: pending });
+          this.$store.commit('SET_PENDING_TXS', pending);
+          if (type === 'tip') this.$router.push({ name: 'success-tip', params: { amount, domain } });
+          if (type === 'spend') this.$router.push({ name: 'send', params: { redirectstep: 3, successtx: mined } });
+        })
+      );
+    },
   },
 };
 </script>
