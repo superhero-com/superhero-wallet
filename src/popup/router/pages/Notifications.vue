2430ccc04abf3dcd5855c4f83ba77751be35d309
mradkov <milenradkov@me.com>
2020-03-23 10:22:09 +0200

    fix closing component tag issue with dynamic component

--- src/popup/router/pages/Notifications.vue
+++ src/popup/router/pages/Notifications.vue
@@ -3,11 +3,10 @@
     <ae-list class="noti-list">
       <ae-list-item fill="neutral" v-for="(noti, i) in notifications" :key="i" class="noti">
         <img src="../../../icons/icon_48.png" />
-        <Component
-          :is="noti.route ? 'RouterLink' : 'span'"
-          :to="noti.route"
-        >
-          {{ noti.content }}
+        <span>
+          <Component :is="noti.route ? 'RouterLink' : 'span'" :to="noti.route">
+            {{ noti.content }}
+          </Component>
         </span>
       </ae-list-item>
     </ae-list>
