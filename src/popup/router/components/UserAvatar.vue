aa01c5df8dc2c377a7a8a0445aaeccf839018973
Petar Baykov <petbaik26@gmail.com>
2020-04-08 12:22:17 +0300

    Add jdenticon lib, integrate jdenticon in UserAvatar component

--- src/popup/router/components/UserAvatar.vue
+++ src/popup/router/components/UserAvatar.vue
@@ -1,31 +1,66 @@
 <template>
-  <img :src="avatarUrl" class="user-avatar" :class="size" />
+  <img :src="avatarUrl" class="user-avatar" :class="size" @error="error = true" v-if="!error" />
+  <div v-html="identicon" class="user-identicon" :class="size" v-else />
 </template>
 
 <script>
+import jdenticon from 'jdenticon';
 import { BACKEND_URL } from '../../utils/constants';
+
+const identiconSizes = {
+  normal: 38,
+  lg: 64,
+};
 
 export default {
   props: {
     address: String,
-    size: String,
+    size: {
+      type: String,
+      default: 'normal',
+    },
   },
+  data: () => ({
+    error: false,
+  }),
   computed: {
     avatarUrl() {
       return `${BACKEND_URL}/profile/image/${this.address}`;
+    },
+    identicon() {
+      jdenticon.config = {
+        lightness: {
+          color: [0.4, 1.0],
+          grayscale: [0.5, 1.0],
+        },
+        saturation: {
+          color: 1.0,
+          grayscale: 1.0,
+        },
+        backColor: '#12121bff',
+      };
+
+      return jdenticon.toSvg(this.address, identiconSizes[this.size]);
     },
   },
 };
 </script>
 
-<style scoped>
-.user-avatar {
-  width: 38px;
-  height: 38px;
+<style lang="scss" scoped>
+$normal-size: 38px;
+$lg-size: 64px;
+
+.user-avatar,
+.user-identicon {
+  width: $normal-size;
+  height: $normal-size;
   border-radius: 50%;
-}
-.user-avatar.lg {
-  height: 4rem;
-  width: 4rem;
+  overflow: hidden;
+  display: inline-block;
+
+  &.lg {
+    height: $lg-size;
+    width: $lg-size;
+  }
 }
 </style>
