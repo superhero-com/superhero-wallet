efbc698c06022089711df738bf27388a00b667e3
Denis Davidyuk <denis_davidyuk@hotmail.com>
2020-04-09 19:09:44 +1000

    travis: Enable per-branch deployment

---
+++ scripts/deploy-stage.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+APP_NAME=wallet
+
+echo "${DEPLOY_KNOWN_HOSTS_BASE64}" | base64 --decode >> ~/.ssh/known_hosts
+echo "${DEPLOY_USER_KEY_BASE64}" | base64 --decode > /tmp/user-key
+eval "$(ssh-agent -s)"
+chmod 600 /tmp/user-key
+ssh-add /tmp/user-key
+
+DOMAIN=`echo $TRAVIS_BRANCH | tr '[:punct:]' '-'`
+
+rsync -e "ssh -p 2022" -r -v dist/web/root/* root@z52da5wt.xyz:/data/$APP_NAME/$DOMAIN
+
+URL=$DOMAIN.$APP_NAME.z52da5wt.xyz
+echo "Deployed to $URL"
+curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
+  -d "{\"body\": \"Deployed to [$URL](https://$URL)\"}" \
+  "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/commits/${TRAVIS_COMMIT}/comments"
