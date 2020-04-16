9d536f14e405ac049b71082cf1b3a2cce05c2cc5
Petar Baykov <petbaik26@gmail.com>
2020-04-16 15:57:44 +0300

    Update notifications.js

--- tests/e2e/integration/notifications.js
+++ tests/e2e/integration/notifications.js
@@ -1,4 +1,7 @@
 describe('Tests cases for notifications page and icon', () => {
+  beforeEach(() => {
+    cy.logout().openPopup();
+  });
   it('Have backup seed notification', () => {
     cy.login()
       .get('[data-cy=noti-count]')
@@ -30,12 +33,5 @@ describe('Tests cases for notifications page and icon', () => {
     cy.login({ backupSeed: true })
       .get('[data-cy=noti-count]')
       .should('be.not.visible');
-  });
-
-  it("Click notification icon don't open notifications page", () => {
-    cy.login({ backupSeed: true })
-      .get('[data-cy=noti]')
-      .click()
-      .urlEquals('/account');
   });
 });
