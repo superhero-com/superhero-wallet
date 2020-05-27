bcee2bb125e7ecafe0c188d53848e06e1056c13b
Milen Radkov <milenradkov@me.com>
2020-05-27 10:26:01 +0300

    add web to build zip

--- scripts/build-zip.js
+++ scripts/build-zip.js
@@ -5,38 +5,42 @@ const path = require('path');
 const archiver = require('archiver');
 
 const DEST_DIR = [
-  { 
+  {
     name: 'chrome',
-    dir: path.join(__dirname, '../dist/chrome')
+    dir: path.join(__dirname, '../dist/chrome'),
   },
   {
     name: 'firefox',
-    dir: path.join(__dirname, '../dist/firefox')
-  }
+    dir: path.join(__dirname, '../dist/firefox'),
+  },
+  {
+    name: 'web',
+    dir: path.join(__dirname, '../dist/web'),
+  },
 ];
-const DEST_ZIP_DIR = path.join(__dirname, '../dist-zip'); 
+const DEST_ZIP_DIR = path.join(__dirname, '../dist-zip');
 
 const extractExtensionData = () => {
   const extPackageJson = require('../package.json');
 
   return {
     name: extPackageJson.name,
-    version: extPackageJson.version
-  }
+    version: extPackageJson.version,
+  };
 };
 
 const makeDestZipDirIfNotExists = () => {
-  if(!fs.existsSync(DEST_ZIP_DIR)) {
+  if (!fs.existsSync(DEST_ZIP_DIR)) {
     fs.mkdirSync(DEST_ZIP_DIR);
   }
-}
+};
 
 const buildZip = (src, dist, zipFilename) => {
   console.info(`Building ${zipFilename}...`);
 
-  const archive = archiver('zip', { zlib: { level: 9 }});
+  const archive = archiver('zip', { zlib: { level: 9 } });
   const stream = fs.createWriteStream(path.join(dist, zipFilename));
-  
+
   return new Promise((resolve, reject) => {
     archive
       .directory(src, false)
@@ -49,17 +53,16 @@ const buildZip = (src, dist, zipFilename) => {
 };
 
 const main = () => {
-
   DEST_DIR.forEach((build, index) => {
-    const {name, version} = extractExtensionData();
+    const { name, version } = extractExtensionData();
     const zipFilename = `${name}-${build.name}-v${version}.zip`;
-    
+
     makeDestZipDirIfNotExists();
-  
+
     buildZip(build.dir, DEST_ZIP_DIR, zipFilename)
       .then(() => console.info('OK'))
-      .catch(console.err); 
-  })
+      .catch(console.err);
+  });
 };
 
 main();
