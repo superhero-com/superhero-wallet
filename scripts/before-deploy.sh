82d9fbfc59a4bfef990b24876b9a63fddbfaa55d
Denis Davidyuk <denis_davidyuk@hotmail.com>
2020-06-06 15:32:46 +1000

    Put extensions to release, add artifacts

--- scripts/before-deploy.sh
+++ scripts/before-deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-set -e
+set -ex
 
 if [[ $CNAME != "" ]]; then
   echo $CNAME > dist/web/root/CNAME
 fi
+
+npm run build-zip
+cp -r dist/web/root dist-stage
+mv dist-zip dist-stage/artifacts
--- scripts/deploy-stage.sh
+++ scripts/deploy-stage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -ex
 
 APP_NAME=wallet
 
@@ -11,10 +11,10 @@ ssh-add /tmp/user-key
 
 DOMAIN=`echo $TRAVIS_BRANCH | tr '[:punct:]' '-'`
 
-rsync -e "ssh -p 2022" -r -v dist/web/root/* root@z52da5wt.xyz:/data/$APP_NAME/$DOMAIN
+rsync -e "ssh -p 2022" -r -v dist-stage/* root@z52da5wt.xyz:/data/$APP_NAME/$DOMAIN
 
 URL=$DOMAIN.$APP_NAME.z52da5wt.xyz
 echo "Deployed to $URL"
 curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
-  -d "{\"body\": \"Deployed to [$URL](https://$URL)\"}" \
+  -d "{\"body\": \"Deployed to [$URL](https://$URL), [artifacts](https://$URL/artifacts)\"}" \
   "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/commits/${TRAVIS_COMMIT}/comments"
