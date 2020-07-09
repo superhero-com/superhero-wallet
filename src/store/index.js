37af4f4064dc8f8420a12fc1b2090ad4791d513b
Petar Baykov <petbaik26@gmail.com>
2020-07-09 08:56:26 +0300

    Update src/store/index.js
    Co-authored-by: Denis Davidyuk <denis_davidyuk@hotmail.com>

--- src/store/index.js
+++ src/store/index.js
@@ -130,7 +130,7 @@ export default new Vuex.Store({
         saveErrorLog,
         tourStartBar,
         tokens: { all },
-        invites: { links },
+        invites,
       }),
     ),
     modals,
