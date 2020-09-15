dc01e6cac00b5050a343ab5d3a8005d7b7438f25
Nikita Cedrik <nikita.cedrik@gmail.com>
2020-09-15 16:15:15 +1000

    backend: Add getAllNotifications method

--- src/lib/backend.js
+++ src/lib/backend.js
@@ -54,6 +54,21 @@ export default class Backend {
 
   static getProfile = async address => backendFetch(`profile/${address}`);
 
+  static async getAllNotifications(address, signCb) {
+    const responseChallenge = await backendFetch(`notification/user/${address}`);
+    const signedChallenge = await signCb(responseChallenge.challenge);
+
+    const respondChallenge = {
+      challenge: responseChallenge.challenge,
+      signature: signedChallenge,
+    };
+    const url = new URL(`${BACKEND_URL}/notification/user/${address}`);
+    Object.keys(respondChallenge).forEach(key =>
+      url.searchParams.append(key, respondChallenge[key]),
+    );
+    return wrapTry(fetch(url.toString()));
+  }
+
   static sendProfileData = async postParam =>
     backendFetch('profile', {
       method: 'post',
