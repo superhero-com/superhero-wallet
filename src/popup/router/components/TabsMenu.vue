de6752bba5ebda590e059c36c10bd058740cc93f
Stanislav Slavov <stanislavslavov90@gmail.com>
2020-10-23 18:20:25 +0300

    Created reusable tabs component
    Removed title from tabs component
    
    Tabs contain button rather than link
    
    Restructured css selectors

---
+++ src/popup/router/components/TabsMenu.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="tabs-menu">
+    <button
+      :class="{ active: selected === value }"
+      v-for="{ text, value } in tabOptions"
+      :key="value"
+      @click="(selected = value), method($event, value)"
+    >
+      {{ text }}
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    tabOptions: { type: Array, default: null },
+    activeTab: { type: [String, Number], default: '' },
+    method: { type: Function, required: true },
+  },
+  data() {
+    return {
+      selected: this.activeTab,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../../../common/variables';
+
+.tabs-menu {
+  padding: 0 15px;
+  background-color: $black-3;
+
+  button {
+    font-size: 16px;
+    padding: 5.5px 0;
+    margin-right: 17px;
+    text-decoration: none;
+    color: $gray-2;
+    display: inline-block;
+
+    &:hover {
+      color: $gray-1;
+    }
+
+    &.active {
+      color: $accent-color;
+      border-bottom: 1px solid $accent-color;
+    }
+
+    &:last-child {
+      margin-right: 0;
+    }
+  }
+}
+</style>
