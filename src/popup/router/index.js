eedf6a0d2d9963f934255eeaa4628d3b9065f0ea
Denis Davidyuk <denis_davidyuk@hotmail.com>
2020-11-13 09:53:02 +0300

    Don't register Modal component globally

--- src/popup/router/index.js
+++ src/popup/router/index.js
@@ -8,7 +8,6 @@ import routes from './routes';
 import '@aeternity/aepp-components-3/dist/aepp.components.css';
 import LoaderComponent from './components/Loader';
 import Button from './components/Button';
-import ModalComponent from './components/Modal';
 
 import * as helper from '../utils/helper';
 import store from '../../store';
@@ -27,7 +26,6 @@ Vue.use(VueClipboard);
 Vue.use(Components);
 Vue.use(VueTour);
 Vue.component('Loader', LoaderComponent);
-Vue.component('Modal', ModalComponent);
 Vue.component('Button', Button);
 
 const router = new VueRouter({
