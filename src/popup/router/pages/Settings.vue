c5d71291a8d508ba4337db8c568505ef8e20d90d
Denis Davidyuk <denis_davidyuk@hotmail.com>
2020-11-21 12:48:44 +0300

    Settings: Use mapMutations

--- src/popup/router/pages/Settings.vue
+++ src/popup/router/pages/Settings.vue
@@ -26,24 +26,18 @@
         :title="$t('pages.settings.tabRemoveAccount')"
         :info="$t('pages.settings.tabRemoveAccountSmall')"
       />
-
       <PanelItem
         :title="$t('pages.settings.tabSaveErrorLog')"
         :info="$t('pages.settings.tabSaveErrorLogSmall')"
       >
-        <CheckBox
-          slot="content"
-          :value="saveErrorLog"
-          name="saveErrorLog"
-          @input="(val) => $store.commit('setSaveErrorLog', val)"
-        />
+        <CheckBox slot="content" :value="saveErrorLog" @input="setSaveErrorLog" />
       </PanelItem>
     </Panel>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex';
+import { mapState, mapMutations } from 'vuex';
 import removeAccountMixin from '../../../mixins/removeAccount';
 import CheckBox from '../components/CheckBox';
 import Panel from '../components/Panel';
@@ -53,5 +47,6 @@ export default {
   mixins: [removeAccountMixin],
   components: { CheckBox, Panel, PanelItem },
   computed: mapState(['saveErrorLog']),
+  methods: mapMutations(['setSaveErrorLog']),
 };
 </script>
