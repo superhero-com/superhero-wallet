691cfa3989d19a476d552438dd4d9bf6e19472c8
Nikita Cedrik <nikita.cedrik@gmail.com>
2020-12-07 19:12:19 +1000

    Tour: Remove unused properties in steps

--- src/popup/router/components/Tour.vue
+++ src/popup/router/components/Tour.vue
@@ -95,7 +95,6 @@ export default {
         },
       },
       {
-        route: '/account',
         target: '.tour__step2 .button-content',
         step: 2,
         params: {
@@ -119,7 +118,6 @@ export default {
         },
       },
       {
-        route: '/account',
         target: '.tour__step5 .button-content',
         step: 5,
         params: {
