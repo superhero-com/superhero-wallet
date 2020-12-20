1a631eef385760ca24430ade11b4b7154be59992
CedrikNikita <nikita.cedrik@gmail.com>
2020-12-20 14:55:01 +1000

    Loader: Realign styles

--- src/popup/router/components/Loader.vue
+++ src/popup/router/components/Loader.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="loading" data-cy="loader">
+  <div class="loader" data-cy="loader">
     <div class="center">
-      <span v-if="content != ''">{{ content }}</span>
-      <br v-if="content != ''" />
-      <ae-loader v-if="size == 'small'" />
+      <span v-if="content !== ''">{{ content }}</span>
+      <br v-if="content !== ''" />
+      <ae-loader v-if="size === 'small'" />
     </div>
-    <transition name="fadeOut" v-if="size == 'big'">
-      <span v-if="type == 'transparent'" class="main-loader main-loader-transparent">
+    <transition v-if="size === 'big'">
+      <span v-if="type === 'transparent'" class="main-loader main-loader-transparent">
         <ae-loader />
       </span>
       <Welcome v-else class="main-loader main-loader-solid" />
@@ -32,57 +32,54 @@ export default {
 <style lang="scss" scoped>
 @import '../../../styles/variables';
 
-.fadeOut-enter-active,
-.fadeOut-leave-active {
-  transition: all 0.5s ease-in-out;
-}
-
-.fadeOut-leave-to {
-  opacity: 0;
-}
-
-.ae-loader {
-  border: 0.2em solid $secondary-color !important;
-  border-left-color: transparent !important;
-  border-right-color: transparent !important;
-}
-
-.main-loader {
-  position: fixed;
-  width: 100%;
-  height: 100%;
-  background-color: $bg-color;
-  top: 0;
-  z-index: 8;
+.loader {
+  .center {
+    position: relative;
+    z-index: 5;
+    word-break: break-word;
+  }
 
   .ae-loader {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    margin: -1.5em;
-    width: 3em !important;
-    height: 3em !important;
-    border-radius: 3em !important;
+    border: 0.2em solid $secondary-color;
+    border-left-color: transparent;
+    border-right-color: transparent;
   }
 
-  &.main-loader-transparent {
-    opacity: 0.6;
+  .main-loader {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    background-color: $bg-color;
+    top: 0;
+    z-index: 8;
+
+    .ae-loader {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      margin: -1.5em;
+      width: 3em;
+      height: 3em;
+      border-radius: 3em;
+    }
+
+    &.main-loader-transparent {
+      opacity: 0.6;
+    }
+
+    &.main-loader-solid {
+      padding-top: 50px;
+      padding-top: calc(50px + env(safe-area-inset-top));
+    }
+
+    &.v-enter-active,
+    &.v-leave-active {
+      transition: all 0.5s ease-in-out;
+    }
+
+    &.v-leave-to {
+      opacity: 0;
+    }
   }
-
-  &.main-loader-solid {
-    padding-top: 50px;
-    padding-top: calc(50px + env(safe-area-inset-top));
-  }
-}
-
-.loader .loading {
-  width: 32px;
-  margin-right: 15px;
-}
-
-.center {
-  position: relative;
-  z-index: 5;
-  word-break: break-word;
 }
 </style>
