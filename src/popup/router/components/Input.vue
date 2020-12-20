dedb8dd42bf56bfc62c630d4a6575f474e50866a
CedrikNikita <nikita.cedrik@gmail.com>
2020-12-20 10:42:43 +0500

    Input: Realign styles

--- src/popup/router/components/Input.vue
+++ src/popup/router/components/Input.vue
@@ -43,87 +43,89 @@ export default {
 <style lang="scss" scoped>
 @import '../../../styles/variables';
 
-input.input {
-  display: block;
-  width: 100%;
-  border-radius: 5px;
-  border: 2px solid $border-color !important;
-  background: $input-bg-color !important;
-  padding: 10px;
-  margin-bottom: 22px;
-  color: $white-1 !important;
-  font-size: 14px;
-  min-height: 35px;
-  margin-left: auto;
-  margin-right: auto;
+.input-wrapper {
+  &.input-group {
+    width: 85%;
+    position: relative;
+    display: inline-block;
+    margin: 8px auto;
 
-  &:focus {
-    border-color: $input-focus-color !important;
+    .input {
+      padding-right: 50px;
+      margin-bottom: 0;
+    }
   }
 
-  &.has-error {
-    border-color: $input-error-color !important;
-  }
-
-  &.sm {
+  .label {
     font-size: 14px;
+    margin: 4px 0;
+    display: block;
+    font-weight: normal;
+
+    &.label-right {
+      position: absolute;
+      right: 9px;
+      margin: 0;
+      top: 17px;
+      transform: translateY(-50%);
+      -ms-transform: translateY(-50%);
+      -webkit-transform: translateY(-50%);
+      opacity: 0.6;
+    }
   }
 
-  &.xsm {
-    font-size: 11px;
+  input.input {
+    display: block;
+    width: 100%;
+    border-radius: 5px;
+    border: 2px solid $border-color;
+    background: $input-bg-color;
+    padding: 10px;
+    margin-bottom: 22px;
+    color: $white-1;
+    font-size: 14px;
+    min-height: 35px;
+    margin-left: auto;
+    margin-right: auto;
+
+    &:focus {
+      border-color: $input-focus-color;
+    }
+
+    &.has-error {
+      border-color: $input-error-color;
+    }
+
+    &.sm {
+      font-size: 14px;
+    }
+
+    &.xsm {
+      font-size: 11px;
+    }
+
+    &.big {
+      font-size: 2rem;
+      font-weight: bold;
+      margin-bottom: 0;
+      height: auto;
+      width: 180px;
+      margin-right: 15px;
+      padding: 3px 10px;
+    }
+
+    &.m-0 {
+      margin: 0;
+    }
+
+    &[type='number'] {
+      -moz-appearance: textfield;
+    }
+
+    &[type='number']::-webkit-outer-spin-button,
+    &[type='number']::-webkit-inner-spin-button {
+      -webkit-appearance: none;
+    }
   }
-
-  &.big {
-    font-size: 2rem;
-    font-weight: bold;
-    margin-bottom: 0;
-    height: auto;
-    width: 180px;
-    margin-right: 15px;
-    padding: 3px 10px;
-  }
-
-  &.m-0 {
-    margin: 0 !important;
-  }
-
-  &[type='number'] {
-    -moz-appearance: textfield;
-  }
-
-  &[type='number']::-webkit-outer-spin-button,
-  &[type='number']::-webkit-inner-spin-button {
-    -webkit-appearance: none;
-  }
-}
-
-.label {
-  font-size: 14px;
-  margin: 4px 0;
-  display: block;
-  font-weight: normal;
-}
-
-.input-group {
-  width: 85%;
-  position: relative;
-  display: inline-block;
-  margin: 8px auto;
-
-  .input {
-    padding-right: 50px;
-    margin-bottom: 0;
-  }
-}
-
-.label-right {
-  position: absolute;
-  right: 9px;
-  margin: 0;
-  top: 17px;
-  transform: translateY(-50%);
-  -ms-transform: translateY(-50%);
-  -webkit-transform: translateY(-50%);
-  opacity: 0.6;
 }
 </style>
