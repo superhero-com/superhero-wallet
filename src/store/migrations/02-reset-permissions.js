0cbbd660618eccd31639074fdf9a9a599e8537d5
Dmitry Kostin <etharner@gmail.com>
2020-12-22 02:10:29 +1000

    Set default domains permissions on migration

--- src/store/migrations/02-reset-permissions.js
+++ src/store/migrations/02-reset-permissions.js
@@ -1,5 +1,17 @@
 export default async (state) => {
   const newState = { ...state };
-  newState.permissions = {};
+  newState.permissions = Object.fromEntries(
+    ['superhero.com', 'localhost'].map((domain) => [
+      domain,
+      {
+        addresses: [],
+        address: true,
+        messageSign: true,
+        transactionSignLimit: 50,
+        transactionSignLimitLeft: 50,
+        transactionSignFirstAskedOn: null,
+      },
+    ]),
+  );
   return newState;
 };
