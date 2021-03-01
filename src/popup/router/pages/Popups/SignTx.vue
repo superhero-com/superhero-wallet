b6315800acfe397407faa7aff47804ae185300d8
Denis Davidyuk <denis_davidyuk@hotmail.com>
2021-03-01 23:44:01 +0300

    fix(sign-tx): disable not working amount editor

--- src/popup/router/pages/Popups/SignTx.vue
+++ src/popup/router/pages/Popups/SignTx.vue
@@ -3,30 +3,24 @@
     <SignAccountIdenticons v-bind="transaction" />
 
     <SignDetailsList :transaction="transaction">
-      <AmountSend v-model="tx.amount" slot="custom-amount" />
+      <AmountSend :value="transaction && transaction.amount" slot="custom-amount" readonly />
     </SignDetailsList>
 
     <div class="button-fixed">
-      <Button dark half @click="cancel" :disabled="!props.reject" data-cy="deny">
+      <Button dark half @click="() => reject()" data-cy="deny">
         {{ $t('pages.signTransaction.reject') }}
       </Button>
-      <Button
-        half
-        @click="sign"
-        :disabled="Number.isNaN(+tx.amount) || tx.amount < 0 || !props.resolve"
-        data-cy="accept"
-      >
+      <Button half @click="() => resolve()" data-cy="accept">
         {{ $t('pages.signTransaction.confirm') }}
       </Button>
     </div>
-    <Loader v-if="loading" />
   </div>
 </template>
 
 <script>
 import { OBJECT_ID_TX_TYPE } from '@aeternity/aepp-sdk/es/tx/builder/schema';
 import { TxBuilder } from '@aeternity/aepp-sdk/es';
-import { aettosToAe, aeToAettos } from '../../../utils/helper';
+import { aettosToAe } from '../../../utils/helper';
 import Button from '../../components/Button';
 import AmountSend from '../../components/AmountSend';
 import getPopupProps from '../../../utils/getPopupProps';
@@ -37,57 +31,25 @@ export default {
   components: { Button, AmountSend, SignAccountIdenticons, SignDetailsList },
   data() {
     return {
-      props: {},
-      loading: false,
-      unpackedTx: null,
-      tx: {
-        amount: 0,
-      },
+      resolve: null,
+      reject: null,
+      transaction: null,
     };
   },
   async mounted() {
-    this.props = await getPopupProps();
-    this.unpackedTx = TxBuilder.unpackTx(this.props.action.params.tx);
-    if (this.txObject.amount >= 0) this.tx.amount = +aettosToAe(this.txObject.amount);
-  },
-  computed: {
-    txObject() {
-      return this.unpackedTx ? this.unpackedTx.tx : {};
-    },
-    totalSpend() {
-      const amount = this.tx.amount || 0;
-      return (parseFloat(amount) + parseFloat(+aettosToAe(this.txObject.fee || 0))).toFixed(7);
-    },
-    transaction() {
-      return {
-        ...this.txObject,
-        fee: +aettosToAe(this.txObject.fee || 0),
-        txType: OBJECT_ID_TX_TYPE[this.txObject.tag],
-        total: this.totalSpend,
-        nameFee: this.txObject.nameFee && +aettosToAe(this.txObject.nameFee),
-      };
-    },
-  },
-  methods: {
-    cancel() {
-      this.props.reject(false);
-    },
-    async sign() {
-      const { tx } = TxBuilder.buildTx(
-        {
-          ...this.unpackedTx.tx,
-          ...this.tx,
-          amount: +aeToAettos(this.tx.amount || 0),
-        },
-        OBJECT_ID_TX_TYPE[this.txObject.tag],
-      );
-      if (parseFloat(this.tx.amount) !== +aettosToAe(this.unpackedTx.tx.amount || 0)) {
-        this.loading = true;
-        this.props.resolve(tx);
-      } else {
-        this.props.resolve();
-      }
-    },
+    const { action, resolve, reject } = await getPopupProps();
+    Object.assign(this, { resolve, reject });
+    const unpackedTx = TxBuilder.unpackTx(action.params.tx).tx;
+    const amount = +aettosToAe(unpackedTx.amount);
+    const fee = +aettosToAe(unpackedTx.fee);
+    this.transaction = {
+      ...unpackedTx,
+      amount,
+      fee,
+      txType: OBJECT_ID_TX_TYPE[unpackedTx.tag],
+      total: (amount + fee).toFixed(7),
+      nameFee: unpackedTx.nameFee && +aettosToAe(unpackedTx.nameFee),
+    };
   },
 };
 </script>
