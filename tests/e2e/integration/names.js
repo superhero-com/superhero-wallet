fa9a5fddf4b4bbe7ead9df369fe61ed7c438f41f
Nikita Cedrik <nikita.cedrik@gmail.com>
2021-03-03 17:12:23 +1000

    fix: adjust TruncateMid component

--- tests/e2e/integration/names.js
+++ tests/e2e/integration/names.js
@@ -1,10 +1,11 @@
-const name = 'test.chain';
+const name = 'test1.chain';
+const resultName = 'test1\u200E.chain';
 describe('Test cases for names', () => {
   it('Contains .chain name in sidebar menu and account info', () => {
     cy.login({ name })
       .openMenu()
       .get('[data-cy=chain-name]')
-      .truncateStringShouldContain('[data-cy=chain-name] .truncate-mid', name)
-      .truncateStringShouldContain('[data-cy=account-name] .truncate-mid', name);
+      .truncateStringShouldContain('[data-cy=chain-name] .truncate-mid', resultName)
+      .truncateStringShouldContain('[data-cy=account-name] .truncate-mid', resultName);
   });
 });
