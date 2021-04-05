de8e4c8cfa7197637a88388ff946ac1a6bef4003
imollov <ivomollov@gmail.com>
2021-04-05 15:58:02 +0300

    refactor(plate): remove negative margin

--- src/popup/router/components/Plate.vue
+++ src/popup/router/components/Plate.vue
@@ -12,7 +12,6 @@
   top: 48px;
   top: calc(48px + env(safe-area-inset-top));
   z-index: 1;
-  margin-left: -1px;
   box-shadow: rgba($color-border, 0.5) 0 1px;
   border-radius: 0 0 10px 10px;
   background: linear-gradient(180deg, $color-bg-3 0%, $color-black 100%);
