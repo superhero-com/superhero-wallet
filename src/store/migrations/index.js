86fa6ebf23a70863d00cf50b330a197ba9d85e00
Nikita Cedrik <nikita.cedrik@gmail.com>
2021-05-21 16:44:38 +1000

    chore(migrations): add addNewFields migration

--- src/store/migrations/index.js
+++ src/store/migrations/index.js
@@ -2,9 +2,11 @@ import runMigrations, { registerMigration } from './runner';
 import collectState from './00-collect-state';
 import setDefaultNames from './01-set-default-names';
 import resetPermissions from './02-reset-permissions';
+import addNewFields from './03-add-new-fields';
 
 registerMigration(collectState);
 registerMigration(setDefaultNames);
 registerMigration(resetPermissions);
+registerMigration(addNewFields);
 
 export default runMigrations;
