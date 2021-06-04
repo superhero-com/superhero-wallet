a959abe3af37e850ef13cca80b179e03a671afdd
etharner <etharner@gmail.com>
2021-06-04 23:50:29 +1000

    chore(showcase): latest

--- src/popup/utils/parseChangelog.js
+++ src/popup/utils/parseChangelog.js
@@ -1,10 +1,14 @@
 export default async () => {
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+
   const changelogUrl = (await (await fetch(
-    'https://api.github.com/repos/aeternity/superhero-wallet/contents/CHANGELOG.md?ref=master',
-    { headers: { 'Content-Type': 'application/json' } },
-  )).json()).git_url;
-  const changelogFull = atob((await (await fetch(changelogUrl)).json()).content);
-  const versionsChangelog = await [...changelogFull.matchAll(/\[(\d+\.\d+\.\d+)\].*?\n(.*?)(?=\[\d+\.\d+\.\d+\])/sg)].slice(0, 2)
+    'http://localhost:5000/CHANGELOG.md',
+    { headers },
+  )).text());
+  // const changelogFull = atob((await (await fetch(changelogUrl, { headers })).json()).content);
+  const versionsChangelog = await [...changelogUrl.matchAll(/\[(\d+\.\d+\.\d+)\].*?\n(.*?)(?=\[\d+\.\d+\.\d+\])/sg)].slice(0, 3)
     .reduce(async (vcl, [, version, versionChangelog]) => ({
       ...await vcl,
       [version]: await [...versionChangelog.matchAll(/\s+### ([\w ]+)\s+(.*?)(?=\s{2,})/sg)]
@@ -13,13 +17,14 @@ export default async () => {
           [topic]: await Promise.all([...details.matchAll(/\[[\w\d]+\](\(.*?)\)/sg)]
             .map(async ([, commit]) => {
               const apiUrl = `https://api.github.com/repos/aeternity/superhero-wallet/commits/${commit.substr(commit.lastIndexOf('/') + 1)}/pulls`;
-              const prs = (await (await fetch(apiUrl, { headers: { 'Content-Type': 'application/json', Accept: 'application/vnd.github.groot-preview+json' } }))
+              const prs = (await (await fetch(apiUrl, { headers: { ...headers, Accept: 'application/vnd.github.groot-preview+json' } }))
                 .json()).sort((a, b) => new Date(a.created_at) - new Date(b.created_at));
               let issuesImgs = [];
+              if (!prs[0]) return { undefined: [] };
               const otherImgs = [...[...prs[0].body.matchAll(/\[image\]\((.*?)\)/g)].map((i) => i[1]), ...[...prs[0].body.matchAll(/https:.*?\.(png|jpg|gif)/g)].map((i) => i[0])];
               if (topic !== 'Features') {
                 const closesIssues = [...prs[0].body.matchAll(/[Closes|closes|Close|close|Fixes|fixes|Fix|fix]\s+#(\d+)/g)].map(([, issue]) => issue);
-                const issues = await Promise.all(closesIssues.map(async (i) => (await fetch(`https://api.github.com/repos/aeternity/superhero-wallet/issues/${i}`, { headers: { 'Content-Type': 'application/json' } })).json()));
+                const issues = await Promise.all(closesIssues.map(async (i) => (await fetch(`https://api.github.com/repos/aeternity/superhero-wallet/issues/${i}`, { headers })).json()));
                 issuesImgs = issues.reduce((iss, i) => [...iss, ...i.body.matchAll(/<img.*>/g)], []);
               }
               const allImgs = [...[...prs[0].body.matchAll(/<img.*>/g), ...issuesImgs].map((i) => i[0].match(/src="(.*)"/)[1]), ...otherImgs];
