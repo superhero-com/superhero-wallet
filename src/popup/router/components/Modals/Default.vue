c8a62554a1d5c179b397c2d4aa28aba31a385d65
imollov <ivomollov@gmail.com>
2021-06-25 11:49:29 +0300

    refactor: extract StatusIcon component

--- src/popup/router/components/Modals/Default.vue
+++ src/popup/router/components/Modals/Default.vue
@@ -1,15 +1,13 @@
 <template>
   <Modal
-    class="default"
     close
     v-on="{ close: close || resolve }"
   >
     <template slot="header">
-      <Component
-        :is="Icon"
-        v-if="Icon"
+      <StatusIcon
+        v-if="icon"
+        :status="icon"
         class="icon"
-        :class="icon"
       />
       {{ title }}
     </template>
@@ -40,15 +38,10 @@
 <script>
 import Modal from '../Modal';
 import Button from '../Button';
-import TimesCircle from '../../../../icons/times-circle.svg?vue-component';
-import QuestionCircle from '../../../../icons/question-circle.svg?vue-component';
-import Alert from '../../../../icons/alert.svg?vue-component';
-import Warning from '../../../../icons/warning.svg?vue-component';
-import CheckCircle from '../../../../icons/check-circle.svg?vue-component';
-import NotSecure from '../../../../icons/not-secure.svg?vue-component';
+import StatusIcon from '../StatusIcon';
 
 export default {
-  components: { Modal, Button },
+  components: { Modal, Button, StatusIcon },
   props: {
     resolve: { type: Function, required: true },
     close: { type: Function, default: null },
@@ -57,51 +50,5 @@ export default {
     type: { type: String, default: '' },
     icon: { type: String, default: '' },
   },
-  computed: {
-    Icon() {
-      switch (this.icon) {
-        case 'critical':
-          return TimesCircle;
-        case 'alert':
-          return Alert;
-        case 'warning':
-          return Warning;
-        case 'info':
-          return QuestionCircle;
-        case 'success':
-          return CheckCircle;
-        case 'not-secure':
-          return NotSecure;
-        default:
-          return null;
-      }
-    },
-  },
 };
 </script>
-
-<style lang="scss" scoped>
-@use '../../../../styles/variables';
-
-.default {
-  .icon {
-    &.alert,
-    &.critical {
-      color: variables.$color-error;
-    }
-
-    &.warning,
-    &.not-secure {
-      color: variables.$color-warning;
-    }
-
-    &.info {
-      color: variables.$color-blue;
-    }
-
-    &.success {
-      color: variables.$color-green;
-    }
-  }
-}
-</style>
--- src/popup/router/components/UrlStatus.vue
+++ src/popup/router/components/UrlStatus.vue
@@ -1,20 +1,18 @@
 <template>
-  <Component
-    :is="data.Icon"
-    class="url-status"
-    :class="data.icon"
+  <StatusIcon
+    v-if="data"
+    :status="data.icon"
     @click="showModal"
   />
+  <Default v-else />
 </template>
 
 <script>
-import Alert from '../../../icons/alert.svg?vue-component';
-import Warning from '../../../icons/warning.svg?vue-component';
-import CheckCircle from '../../../icons/check-circle.svg?vue-component';
-import NotSecure from '../../../icons/not-secure.svg?vue-component';
+import StatusIcon from './StatusIcon';
 import Default from '../../../icons/badges/default.svg?vue-component';
 
 export default {
+  components: { StatusIcon, Default },
   props: {
     status: { type: String, required: true },
   },
@@ -22,31 +20,15 @@ export default {
     data() {
       switch (this.status) {
         case 'verified':
-          return {
-            Icon: CheckCircle,
-            icon: 'success',
-            content: this.$t('modals.verified'),
-          };
+          return { icon: 'success', content: this.$t('modals.verified') };
         case 'blacklisted':
-          return {
-            Icon: Alert,
-            icon: 'alert',
-            content: this.$t('modals.blacklisted'),
-          };
+          return { icon: 'alert', content: this.$t('modals.blacklisted') };
         case 'not-secure':
-          return {
-            Icon: NotSecure,
-            icon: 'not-secure',
-            content: this.$t('modals.not-secure'),
-          };
+          return { icon: 'not-secure', content: this.$t('modals.not-secure') };
         case 'not-verified':
-          return {
-            Icon: Warning,
-            icon: 'warning',
-            content: this.$t('modals.not-verified'),
-          };
+          return { icon: 'warning', content: this.$t('modals.not-verified') };
         case 'default':
-          return { Icon: Default };
+          return null;
         default:
           throw new Error(`Unknown url status: ${this.status}`);
       }
@@ -54,38 +36,19 @@ export default {
   },
   methods: {
     showModal() {
-      if (this.status !== 'default') {
-        this.$store.dispatch('modals/open', {
-          name: 'default',
-          title: this.data.content.title,
-          msg: this.data.content.msg,
-          icon: this.data.icon,
-        });
-      }
+      this.$store.dispatch('modals/open', {
+        name: 'default',
+        title: this.data.content.title,
+        msg: this.data.content.msg,
+        icon: this.data.icon,
+      });
     },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-@use '../../../styles/variables';
-
-.url-status {
-  width: 18px;
-  height: 18px;
+.status-icon {
   cursor: pointer;
-
-  &.alert {
-    color: variables.$color-error;
-  }
-
-  &.warning,
-  &.not-secure {
-    color: variables.$color-warning;
-  }
-
-  &.success {
-    color: variables.$color-green;
-  }
 }
 </style>
