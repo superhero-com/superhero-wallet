138700a86849bbc11d24c4b19721e7c2b2a7be23
Nikita Cedrik <nikita.cedrik@gmail.com>
2021-07-02 18:42:22 +1000

    chore(migrations): rename migration

--- src/store/migrations/index.js
+++ src/store/migrations/index.js
@@ -2,11 +2,11 @@ import runMigrations, { registerMigration } from './runner';
 import collectState from './00-collect-state';
 import setDefaultNames from './01-set-default-names';
 import resetPermissions from './02-reset-permissions';
-import addNewFields from './03-add-new-fields';
+import addFungibleTokensNewFields from './03-add-fungible-tokens-new-fields';
 
 registerMigration(collectState);
 registerMigration(setDefaultNames);
 registerMigration(resetPermissions);
-registerMigration(addNewFields);
+registerMigration(addFungibleTokensNewFields);
 
 export default runMigrations;
