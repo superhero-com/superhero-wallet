0a637a8bf9fa54f308aa0818102677261b69d764
Nikita Cedrik <nikita.cedrik@gmail.com>
2021-07-19 14:54:31 +1000

    feat(routes): add tip redirection

--- src/popup/router/routes/index.js
+++ src/popup/router/routes/index.js
@@ -214,6 +214,14 @@ export default [
     }],
   },
   {
+    path: '/tip',
+    redirect: '/tips',
+  },
+  {
+    path: '/tip/*',
+    redirect: '/tips/*',
+  },
+  {
     path: '/retip',
     component: Retip,
     meta: {
