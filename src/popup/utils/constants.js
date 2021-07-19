693bbc079ba4377c6610141cecdbae63c8d8c136
etharner <etharner@gmail.com>
2021-07-19 16:45:50 +1000

    chore(ios-link): change ios link from testflight to app store

--- src/popup/utils/constants.js
+++ src/popup/utils/constants.js
@@ -126,4 +126,4 @@ export const APP_LINK_WEB = 'https://wallet.superhero.com';
 export const APP_LINK_CHROME = 'https://chrome.google.com/webstore/detail/superhero/mnhmmkepfddpifjkamaligfeemcbhdne';
 export const APP_LINK_FIREFOX = 'https://addons.mozilla.org/en-US/firefox/addon/superhero-wallet';
 export const APP_LINK_ANDROID = 'https://play.google.com/store/apps/details?id=com.superhero.cordova';
-export const APP_LINK_IOS = 'https://testflight.apple.com/join/3o5r4dQQ';
+export const APP_LINK_IOS = 'https://apps.apple.com/us/app/superhero-wallet/id1502786641';
