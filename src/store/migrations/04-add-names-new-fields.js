cc8dfbbe6eb51e4677589f9a1fb1d74aa652ec64
Nikita Cedrik <nikita.cedrik@gmail.com>
2021-08-19 16:58:05 +1000

    fix(migrations): add missing new names field

--- src/store/migrations/04-add-names-new-fields.js
+++ src/store/migrations/04-add-names-new-fields.js
@@ -1,5 +1,6 @@
 export default (state) => {
   const newState = { ...state };
   if (!newState.names.preferred) newState.names.preferred = {};
+  if (!newState.names.auctions) newState.names.auctions = {};
   return newState;
 };
