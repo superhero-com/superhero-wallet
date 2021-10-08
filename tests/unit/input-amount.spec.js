41272efdf3c835f5f9870ea7d106c47ee9531c76
Nikita Cedrik <nikita.cedrik@gmail.com>
2021-10-08 13:04:27 +1000

    test(input-amount): convert test from e2e to unit

---
+++ tests/unit/input-amount.spec.js
@@ -0,0 +1,85 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import { mount, createLocalVue } from '@vue/test-utils';
+import InputAmount from '../../src/popup/router/components/InputAmount.vue';
+
+Object.assign(Vue.prototype, {
+  $t: () => 'locale-specific-text',
+});
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+const store = new Vuex.Store({
+  getters: {
+    currentCurrencyRate: () => 3,
+    formatCurrency: () => (value) => (+value).toFixed(2),
+  },
+  modules: {
+    fungibleTokens: {
+      state: {
+        token: null,
+      },
+      namespaced: true,
+      getters: {
+        selectedToken: ({ token }) => token,
+      },
+      mutations: {
+        setToken(state, token) {
+          state.token = token;
+        },
+      },
+    },
+  },
+});
+
+describe('InputAmount', () => {
+  [{
+    name: 'input zero',
+    value: 0,
+    displayed: 0,
+    currency: 0,
+  },
+  {
+    name: 'input int',
+    value: 1,
+    displayed: 1,
+    currency: 3,
+  },
+  {
+    name: 'input float',
+    value: 0.1234567890,
+    displayed: 0.1234567890,
+    currency: 0.37,
+  },
+  {
+    name: 'input in exponential notation',
+    value: '1e2',
+    displayed: '1e2',
+    currency: 300,
+  },
+  {
+    name: 'input negative int',
+    error: true,
+    value: -1,
+    displayed: -1,
+    currency: -3,
+  },
+  {
+    name: 'input float with token',
+    token: { symbol: 'AR' },
+    value: 0.1234567890,
+    displayed: 0.1234567890,
+    currency: 0,
+  }].forEach((test) => it(test.name, () => {
+    if (test.token) {
+      store.commit('fungibleTokens/setToken', test.token);
+    }
+    const wrapper = mount(InputAmount, { localVue, store, attrs: { value: test.value } });
+    expect(wrapper.find('input').element.value).toBe(test.displayed.toString());
+    expect(wrapper.find('.token').text()).toBe(test.token?.symbol || 'AE');
+    expect(wrapper.find('[data-cy=amount-currency]').text()).toBe(`(${test.currency.toFixed(2)})`);
+    if (test.error) {
+      expect(wrapper.find('.message.error').exists()).toBeTruthy();
+    }
+  }));
+});
