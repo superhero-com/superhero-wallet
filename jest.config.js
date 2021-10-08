fb1e91bed0ae04645a11fa07e842282ec929c44d
Nikita Cedrik <nikita.cedrik@gmail.com>
2021-10-08 11:52:49 +1000

    test(jest-config): add several packages to transpile

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,12 @@
+const packagesToTranspile = [
+  'lodash-es',
+  '@aeternity/aepp-sdk',
+  '@aeternity/hd-wallet',
+];
+
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
+  transformIgnorePatterns: [
+    `node_modules/(?!(${packagesToTranspile.join('|')})/)`,
+  ],
 };
