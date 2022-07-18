f97a76cc5b39a2143ee26dedaaf9618fba038945
CedrikNikita <nikita.cedrik@gmail.com>
2022-07-18 15:13:25 +1000

    refactor: do not poll currencies if not logged in

--- src/store/plugins/pollAccountInfo.js
+++ src/store/plugins/pollAccountInfo.js
@@ -2,6 +2,7 @@ export default async (store) => {
   let pollAvailableTokens;
   let pollTokenBalances;
   let pollDefaultNames;
+  let pollCurrencies;
 
   store.watch(
     (state, { wallet }) => wallet,
@@ -9,10 +10,12 @@ export default async (store) => {
       clearInterval(pollDefaultNames);
       clearInterval(pollAvailableTokens);
       clearInterval(pollTokenBalances);
+      clearInterval(pollCurrencies);
 
       pollDefaultNames = setInterval(() => store.dispatch('names/setDefaults'), 10000);
       pollAvailableTokens = setInterval(() => store.dispatch('fungibleTokens/getAvailableTokens'), 60000);
       pollTokenBalances = setInterval(() => store.dispatch('fungibleTokens/loadTokenBalances'), 10000);
+      pollCurrencies = await store.dispatch('pollCurrencies');
     },
   );
 };
