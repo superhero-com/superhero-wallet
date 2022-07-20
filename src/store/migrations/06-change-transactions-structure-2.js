c462025f42dfb2cbac0159f966a6500ec2f4ea66
CedrikNikita <nikita.cedrik@gmail.com>
2022-07-20 21:59:32 +1000

    fix(mutations): avoid potential problems with missing fields

--- src/store/migrations/06-change-transactions-structure-2.js
+++ src/store/migrations/06-change-transactions-structure-2.js
@@ -1,5 +1,8 @@
 export default (state) => {
   const newState = { ...state };
   if (Array.isArray(newState.transactions.pending)) newState.transactions.pending = {};
+  if (!newState.transactions.tipWithdrawnTransactions) {
+    newState.transactions.tipWithdrawnTransactions = [];
+  }
   return newState;
 };
