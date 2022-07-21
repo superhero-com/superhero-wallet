4124442da0ba07c3051575f3f81e17f8d49fb8b7
CedrikNikita <nikita.cedrik@gmail.com>
2022-07-21 17:53:06 +1000

    refactor: show also retip transactions when filtered by tips

--- src/popup/utils/helper.js
+++ src/popup/utils/helper.js
@@ -147,7 +147,8 @@ export const getBalanceLocalStorage = () => (
 );
 
 export const categorizeContractCallTxObject = (transaction) => {
-  if (transaction.incomplete || (transaction.type === 'tip' && transaction.pending)) {
+  if (transaction.incomplete
+    || ((transaction.tx.function === 'tip' || transaction.tx.function === 'retip') && transaction.pending)) {
     return {
       amount: transaction.amount, token: transaction.tx.contractId, to: transaction.tx.callerId,
     };
