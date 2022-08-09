12a2d60d37ff3848628f67553a1b053fad0e3b93
Paweł Stachel <p.stachel@qarbon.it>
2022-08-09 11:55:54 +0200

    feat: add bullets to account switcher

--- src/popup/utils/constants.js
+++ src/popup/utils/constants.js
@@ -183,7 +183,7 @@ export const CURRENCY_URL = 'https://api.coingecko.com/api/v3/coins/markets?ids=
 
 export const AVATAR_URL = 'https://avatars.z52da5wt.xyz/';
 
-export const DEX_URL = 'https://dex.superhero.com/';
+export const DEX_URL = 'https://aepp.dex.superhero.com';
 
 export const BLOG_CLAIM_TIP_URL = 'https://blog.aeternity.com/superhero-how-to-send-receive-superhero-tips-34971b18c919#024e';
 
