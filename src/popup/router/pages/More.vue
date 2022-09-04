671137a56c3c51d0676acf7e827d5a27218b20d5
badi <badi1wow@gmail.com>
2022-09-04 08:45:17 +0100

    feat: more main screen, new app ui background

--- src/popup/router/pages/More.vue
+++ src/popup/router/pages/More.vue
@@ -1,60 +1,100 @@
 <template>
   <div class="more">
-    <!-- TODO: remove after Plate + AccountSwitcher will be extracted-->
-    <Plate>
-      <AccountSwitcher />
-    </Plate>
-    <div class="navigation">
-      <BoxButton to="/invite">
-        <Invites /> {{ $t('pages.titles.invite') }}
-      </BoxButton>
-      <BoxButton
-        to="/settings"
-        data-cy="settings"
-      >
-        <Settings /> {{ $t('pages.titles.settings') }}
-      </BoxButton>
-      <BoxButton
-        to="/about"
-        data-cy="about"
-      >
-        <About /> {{ $t('pages.titles.about') }}
-      </BoxButton>
-      <BoxButton
-        :to="SIMPLEX_URL"
-        fill="alternative"
-        is-external-link
-      >
-        <Buy />{{ $t('pages.fungible-tokens.buyAe') }}
-      </BoxButton>
-      <BoxButton
-        :to="DEX_URL"
-        is-external-link
-      >
-        <Dex /> {{ $t('pages.more.dex') }}
-      </BoxButton>
-    </div>
+    <PanelItem
+      :to="{ name: 'settings' }"
+      :title="$t('pages.titles.settings')"
+      data-cy="settings"
+    >
+      <template #icon>
+        <Settings />
+      </template>
+    </PanelItem>
+
+    <PanelItem
+      :to="{ name: 'tips-claim' }"
+      :title="$t('pages.titles.claim-tips')"
+      data-cy="tips-claim"
+    >
+      <template #icon>
+        <ClaimTips />
+      </template>
+    </PanelItem>
+
+    <PanelItem
+      :to="{ name: 'invite' }"
+      :title="$t('pages.titles.invite')"
+      data-cy="invite"
+    >
+      <template #icon>
+        <Invites />
+      </template>
+    </PanelItem>
+
+    <!-- TODO FIX URL IN NEW PAGE -->
+    <PanelItem
+      :href="BUG_REPORT_URL"
+      :title="$t('pages.about.reportBug')"
+    >
+      <template #icon>
+        <BugReport />
+      </template>
+    </PanelItem>
+    <PanelItem
+      :href="SIMPLEX_URL"
+      :title="$t('pages.fungible-tokens.buyAe')"
+    >
+      <template #icon>
+        <Buy />
+      </template>
+    </PanelItem>
+    <PanelItem
+      :href="DEX_URL"
+      :title="$t('pages.more.dex')"
+    >
+      <template #icon>
+        <Dex />
+      </template>
+    </PanelItem>
+
+    <!-- TODO FIX URL IN NEW PAGE -->
+    <PanelItem
+      :to="{ name: 'about' }"
+      :title="$t('pages.titles.about')"
+      data-cy="about"
+    >
+      <template #icon>
+        <About />
+      </template>
+    </PanelItem>
   </div>
 </template>
 
 <script>
-import Plate from '../components/Plate.vue';
-import AccountSwitcher from '../components/AccountSwitcher.vue';
-import BoxButton from '../components/BoxButton.vue';
+import PanelItem from '../components/PanelItem.vue';
 import Invites from '../../../icons/invites.svg?vue-component';
 import Settings from '../../../icons/settings.svg?vue-component';
-import About from '../../../icons/circle-i.svg?vue-component';
+import BugReport from '../../../icons/bug-report.svg?vue-component';
+import About from '../../../icons/about.svg?vue-component';
 import Buy from '../../../icons/buy.svg?vue-component';
 import Dex from '../../../icons/dex.svg?vue-component';
-import { DEX_URL, SIMPLEX_URL } from '../../utils/constants';
+import ClaimTips from '../../../icons/claim-tips.svg?vue-component';
+import { DEX_URL, SIMPLEX_URL, BUG_REPORT_URL } from '../../utils/constants';
 
 export default {
   components: {
-    Plate, AccountSwitcher, BoxButton, Invites, Settings, About, Buy, Dex,
+    PanelItem,
+    Invites,
+    Settings,
+    About,
+    Buy,
+    Dex,
+    BugReport,
+    ClaimTips,
   },
   data: () => ({
     SIMPLEX_URL,
     DEX_URL,
+    BUG_REPORT_URL,
   }),
 };
 </script>
@@ -65,15 +105,5 @@ export default {
 .more {
   width: variables.$extension-width;
   margin: 0 auto;
-
-  .navigation {
-    padding: 12px;
-    display: flex;
-    flex-wrap: wrap;
-
-    .box-button {
-      margin: 12px;
-    }
-  }
 }
 </style>
