d01e3f7336fc99b63a5c6a644d9837b7bccb0ea7
badi <badi1wow@gmail.com>
2022-09-04 09:14:06 +0100

    feat: permission setting ui updates

--- src/popup/router/pages/PermissionsSettings.vue
+++ src/popup/router/pages/PermissionsSettings.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="permissions-settings">
-    <Panel v-if="hosts.length">
+    <i18n
+      path="pages.permissions.description"
+      tag="div"
+      class="description"
+    >
+      <a
+        href="https://superhero.com/"
+        target="_blank"
+      >
+        superhero.com
+      </a>
+    </i18n>
+    <template v-if="hosts.length">
       <PanelItem
         v-for="host in hosts"
         :key="host"
@@ -8,7 +20,7 @@
         :to="{ name: 'permissions-details', params: { host } }"
         :title="host"
       />
-    </Panel>
+    </template>
     <div v-else>
       {{ $t('pages.permissions.empty') }}
     </div>
@@ -16,12 +28,10 @@
 </template>
 
 <script>
-import Panel from '../components/Panel.vue';
 import PanelItem from '../components/PanelItem.vue';
 
 export default {
   components: {
-    Panel,
     PanelItem,
   },
   computed: {
@@ -31,3 +41,25 @@ export default {
   },
 };
 </script>
+
+<style lang="scss" scoped>
+  @use '../../../styles/variables';
+  @use '../../../styles/typography';
+
+  .permissions-settings {
+    padding: 6px;
+    .description {
+      color: rgba(variables.$color-white, 0.75);
+      padding: 8px 8px 12px 8px;
+      line-height: 20px;
+
+      a {
+        color: variables.$color-primary;
+        text-decoration: none;
+      }
+
+      @extend %face-sans-14-light;
+    }
+  }
+
+</style>
