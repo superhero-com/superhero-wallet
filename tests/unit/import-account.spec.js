1a7c7efdee9861935de0f1703a41868023ccfff1
Cedrik Nikita <nikita.cedrik@gmail.com>
2022-12-01 18:47:48 +0400

    test(import-account): test seed phrases more than 12 words

--- tests/unit/import-account.spec.js
+++ tests/unit/import-account.spec.js
@@ -20,8 +20,18 @@ describe('ImportAccount', () => { // TODO: rewrite test
     value: 'mystery mystery mystery mystery mystery mystery mystery mystery mystery mystery mystery mystery',
   },
   {
-    name: 'input correct phrase',
+    name: 'input correct seed phrase of 12 words',
     value: 'media view gym mystery all fault truck target envelope kit drop fade',
+    correct: true,
+  },
+  {
+    name: 'input correct seed phrase of 18 words',
+    value: 'lonely asset near deputy child echo biology talk receive pen alcohol habit myth retreat rebel grief twenty nothing',
+    correct: true,
+  },
+  {
+    name: 'input correct seed phrase of 24 words',
+    value: 'draft cruise tenant ride extend rose urban mean sponsor quit friend indoor door mean toe donate journey minute annual rough give giggle motion zebra',
     correct: true,
   },
   {
