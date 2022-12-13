fec08d110a6c67910b84f03cd5a7c21a3efad137
konradodo <k.odo@qarbon.it>
2022-12-13 11:21:53 +0100

    feat: remodeled not found page

--- src/popup/pages/NotFound.vue
+++ src/popup/pages/NotFound.vue
@@ -1,23 +1,97 @@
 <template>
   <div class="not-found">
-    {{ $t('pages.notFound.notFound') }}
-    <BtnMain :to="{ name: 'index' }">
-      {{ $t('pages.notFound.initialPage') }}
-    </BtnMain>
+    <img
+      class="image"
+      :src="NotFoundImage"
+    >
+    <div class="title">
+      {{ $t('pages.notFound.header') }}
+    </div>
+    <div class="description">
+      {{ $t('pages.notFound.description') }}
+    </div>
+    <div class="button-wrapper">
+      <BtnMain
+        class="button"
+        :to="{ name: 'index' }"
+      >
+        {{ $t('pages.notFound.backToHome') }}
+      </BtnMain>
+      <BtnMain
+        class="button"
+        :href="BUG_REPORT_URL"
+        variant="secondary"
+      >
+        <BugIcon class="icon" />
+        {{ $t('pages.notFound.reportBug') }}
+      </BtnMain>
+    </div>
   </div>
 </template>
 
 <script>
+import {
+  defineComponent,
+} from '@vue/composition-api';
 import BtnMain from '../components/buttons/BtnMain.vue';
+import NotFoundImage from '../../image/not-found.jpg';
+import BugIcon from '../../icons/bug.svg?vue-component';
+import { BUG_REPORT_URL } from '../utils';
 
-export default {
-  components: { BtnMain },
-};
+export default defineComponent({
+  components: { BtnMain, BugIcon },
+  setup() {
+    return {
+      NotFoundImage,
+      BUG_REPORT_URL,
+    };
+  },
+});
+
 </script>
 
 <style lang="scss" scoped>
+@use '../../styles/typography';
+
 .not-found {
-  margin: 20px auto;
   text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: auto;
+  padding: 0 16px;
+
+  .image {
+    margin-top: 88px;
+    width: 156px;
+  }
+
+  .title {
+    @extend %face-sans-22-bold;
+
+    margin-top: 16px;
+  }
+
+  .description {
+    @extend %face-sans-15-regular;
+
+    margin-top: 8px;
+    margin-bottom: 32px;
+  }
+
+  .button-wrapper {
+    width: 100%;
+  }
+
+  .button {
+    width: 100%;
+    margin-bottom: 16px;
+  }
+
+  .icon {
+    margin-right: 4px;
+    width: 20px;
+    height: 20px;
+  }
 }
 </style>
