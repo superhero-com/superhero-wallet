1bac9a69513194ee97d3263ddb21ae9f3c03d13d
Nitin Ramnani <nitinramnani15@gmail.com>
2022-12-21 13:09:26 +0530

    Fixed snapshots

--- tests/unit/Tokens.spec.js
+++ tests/unit/Tokens.spec.js
@@ -1,35 +1,34 @@
-import {shallowMount} from '@vue/test-utils'
-import Tokens from '../../src/popup/components/Tokens.vue'
+import { shallowMount } from '@vue/test-utils';
+import Tokens from '../../src/popup/components/Tokens.vue';
 
 let wrapper;
 
-describe('Tokens',()=>{
-    beforeEach(()=>{
-        wrapper = shallowMount(Tokens,{
-            propsData:{
-                tokens:[{symbol:'test123'},{symbol:'test456'}],
-                vertical:true
-            }
-        })
-    })
+describe('Tokens', () => {
+  beforeEach(() => {
+    wrapper = shallowMount(Tokens, {
+      propsData: {
+        tokens: [{ symbol: 'test123' }, { symbol: 'test456' }],
+        vertical: true,
+      },
+    });
+  });
 
-    it('should match the snapshot',()=>{
-        expect(wrapper.vm).toMatchSnapshot()
-    })
+  it('should match the snapshot', () => {
+    expect(wrapper.html()).toMatchSnapshot();
+  });
 
-    it('should test getAvailableCharLength',async ()=>{
-        const getAvailableCharLength = jest.spyOn(wrapper.vm,'getAvailableCharLength')
-        let length = wrapper.vm.getAvailableCharLength()
-        expect(getAvailableCharLength).toHaveBeenCalled()
-        expect(length).toEqual(5)
-        await wrapper.setProps({symbolLength:11,
-            doubleSymbolLength:5,
-            tokens:[{symbol:'test'},{symbol:'test456'}],
-            vertical:true})
-        length = wrapper.vm.getAvailableCharLength()
-        expect(length).toEqual(7)
-    })
-})
-
-
-
+  it('should test getAvailableCharLength', async () => {
+    const getAvailableCharLength = jest.spyOn(wrapper.vm, 'getAvailableCharLength');
+    let length = wrapper.vm.getAvailableCharLength();
+    expect(getAvailableCharLength).toHaveBeenCalled();
+    expect(length).toEqual(5);
+    await wrapper.setProps({
+      symbolLength: 11,
+      doubleSymbolLength: 5,
+      tokens: [{ symbol: 'test' }, { symbol: 'test456' }],
+      vertical: true,
+    });
+    length = wrapper.vm.getAvailableCharLength();
+    expect(length).toEqual(7);
+  });
+});
