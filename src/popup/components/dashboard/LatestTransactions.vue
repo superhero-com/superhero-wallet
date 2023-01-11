4062d23f8beee8452f0d85c5f06a56555f21abe1
konradodo <k.odo@qarbon.it>
2023-01-11 09:43:11 +0100

    feat: composable for lastTransactions

---
+++ src/popup/components/dashboard/LatestTransactions.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="latest-transaction">
+    <div
+      v-for="transaction in latestTransactions"
+      :key="transaction.tx_hash"
+    >
+      {{ transaction.tx_hash }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+import { useCombinedLatestTransactions } from '../../../composables';
+
+export default defineComponent({
+  name: 'LatestTransactions',
+  setup() {
+    const { latestTransactions } = useCombinedLatestTransactions();
+
+    return {
+      latestTransactions,
+    };
+  },
+});
+</script>
+
+<style scoped>
+.latest-transaction {
+  /*  TODO */
+}
+</style>
