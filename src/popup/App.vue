78fe495fd56c61fe7b69229b97636e13d2e4b191
konradodo <k.odo@qarbon.it>
2023-04-17 11:28:42 +0200

    fix: changing solution about scroll top state

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -156,6 +156,12 @@ export default defineComponent({
       }
     });
 
+    watch(() => root.$route.fullPath, () => {
+      if (innerElement.value) {
+        innerElement.value.scrollTop = 0;
+      }
+    });
+
     onMounted(async () => {
       setDocumentHeight();
       checkExtensionUpdates();
