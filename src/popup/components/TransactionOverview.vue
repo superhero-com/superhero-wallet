222b2dc4e67670928ef15bb5bce86ac017ab32f2
Martin <mkaintas@unicorndomain.gr>
2023-04-24 11:27:13 +0300

    fix: i18n errors when handling arrays of translations

--- src/popup/components/TransactionOverview.vue
+++ src/popup/components/TransactionOverview.vue
@@ -51,7 +51,7 @@ export default defineComponent({
   },
   setup(props) {
     const store = useStore();
-    const { t } = useI18n();
+    const { t, tm } = useI18n();
 
     const name = ref('');
     const ownershipAccount = ref<IAccountLabeled | IAccount | {}>({});
@@ -83,7 +83,7 @@ export default defineComponent({
     }
 
     const preparedTransaction = computed((): TransactionData => {
-      const transactionTypes = t('transaction.type') as unknown as Record<TxType, TranslateResult>;
+      const transactionTypes = tm('transaction.type') as unknown as Record<TxType, TranslateResult>;
 
       const {
         senderId,
