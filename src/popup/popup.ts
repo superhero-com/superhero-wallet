fa59c0b968d91e73ed7cf5f7ccb1dc038c9b616a
ZGiovas <dr.vegapunk21@gmail.com>
2023-06-06 12:55:12 +0300

    feat: init ionic project, configs and update package

--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -1,6 +1,7 @@
 import '@/lib/initPolyfills';
 import '@/protocols/registerAdapters';
 import { createApp } from 'vue';
+import { IonicVue } from '@ionic/vue';
 import store from '../store';
 import router from './router';
 import { i18n } from '../store/plugins/languages';
@@ -11,8 +12,25 @@ import LoaderComponent from './components/Loader.vue';
 
 import '../styles/fullscreen-message.scss';
 
+/* Core CSS required for Ionic components to work properly */
+// import '@ionic/vue/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/vue/css/normalize.css';
+import '@ionic/vue/css/structure.css';
+import '@ionic/vue/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/vue/css/padding.css';
+// // import '@ionic/vue/css/float-elements.css';
+import '@ionic/vue/css/text-alignment.css';
+import '@ionic/vue/css/text-transformation.css';
+import '@ionic/vue/css/flex-utils.css';
+import '@ionic/vue/css/display.css';
+
 registerModals();
 const app = createApp(App);
+app.use(IonicVue);
 app.use(i18n);
 app.use(store);
 app.use(router);
