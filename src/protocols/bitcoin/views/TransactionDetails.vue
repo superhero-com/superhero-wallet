09f71474e339c7ad7ba2d521cfe78bc1be49472d
Martin <mkaintas@unicorndomain.gr>
2023-06-23 10:38:38 +0300

    feat: add ionic routing to account pages

--- src/protocols/bitcoin/views/TransactionDetails.vue
+++ src/protocols/bitcoin/views/TransactionDetails.vue
@@ -1,23 +1,29 @@
 <template>
-  <div class="transaction-details">
-    <Loader v-if="!transaction" />
-    <template v-else>
-      <TransactionDetailsBase
-        :transaction="transaction"
-        :coin-symbol="BTC_SYMBOL"
-        :transaction-fee="transactionFee"
-        :token-symbol="BTC_SYMBOL"
-        :total-amount="totalAmount"
-        :direction="direction"
-        :explorer-url="explorerUrl"
-        :is-local-account-address="isLocalAccountAddress"
-        :hash="hash"
-        :none-ae-coin="tokens"
-        :protocol="PROTOCOL_BITCOIN"
-      />
-    </template>
-    <div />
-  </div>
+  <ion-page>
+    <ion-content
+      class="ion-padding"
+    >
+      <div class="transaction-details">
+        <Loader v-if="!transaction" />
+        <template v-else>
+          <TransactionDetailsBase
+            :transaction="transaction"
+            :coin-symbol="BTC_SYMBOL"
+            :transaction-fee="transactionFee"
+            :token-symbol="BTC_SYMBOL"
+            :total-amount="totalAmount"
+            :direction="direction"
+            :explorer-url="explorerUrl"
+            :is-local-account-address="isLocalAccountAddress"
+            :hash="hash"
+            :none-ae-coin="tokens"
+            :protocol="PROTOCOL_BITCOIN"
+          />
+        </template>
+        <div />
+      </div>
+    </ion-content>
+  </ion-page>
 </template>
 
 <script lang="ts">
@@ -28,6 +34,7 @@ import {
   onMounted,
 } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
+import { IonContent, IonPage } from '@ionic/vue';
 
 import { ProtocolAdapterFactory } from '@/lib/ProtocolAdapterFactory';
 import type { ITransaction } from '@/types';
@@ -41,6 +48,8 @@ import BtcIcon from '@/icons/coin/bitcoin.svg';
 export default defineComponent({
   components: {
     TransactionDetailsBase,
+    IonContent,
+    IonPage,
   },
   setup() {
     const router = useRouter();
