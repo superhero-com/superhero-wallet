de11a42a343a260809d23a0d9743470233eb3145
Martin <mkaintas@unicorndomain.gr>
2023-06-23 15:03:06 +0300

    feat: add ionic routing to multisig pages

--- src/popup/pages/FungibleTokens/TokenTransactions.vue
+++ src/popup/pages/FungibleTokens/TokenTransactions.vue
@@ -1,11 +1,21 @@
 <template>
-  <TransactionList
-    :token-contract-id="contractId"
-    :is-multisig="isMultisig"
-  />
+  <ion-page>
+    <ion-content
+      class="ion-padding"
+    >
+      <TransactionList
+        :token-contract-id="contractId"
+        :is-multisig="isMultisig"
+      />
+    </ion-content>
+  </ion-page>
 </template>
 
 <script lang="ts">
+import {
+  IonContent,
+  IonPage,
+} from '@ionic/vue';
 import { defineComponent } from 'vue';
 import TransactionList from '../../components/TransactionList.vue';
 
@@ -13,6 +23,8 @@ export default defineComponent({
   name: 'TokenTransactions',
   components: {
     TransactionList,
+    IonPage,
+    IonContent,
   },
   props: {
     contractId: { type: String, default: null },
