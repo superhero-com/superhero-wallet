c40f1a49f8a893570cfd104e415e2d04a3132132
ZGiovas <dr.vegapunk21@gmail.com>
2023-07-11 11:23:50 +0300

    refactor: delete unused files and replace cordova configs

--- src/types/ionic.ts
+++
@@ -1,5 +0,0 @@
-// Boilerplate schema for Ionic API available on window object
-export interface IIonic {
-  platformId?: string
-  plugins?: Record<string, any>
-}
--- window.d.ts
+++ window.d.ts
@@ -1,12 +1,10 @@
 import { IPopupConfig } from './src/types';
-import { IIonic } from './src/types/ionic';
 
 export { };
 
 declare global {
   interface Window {
     popupProps?: Partial<IPopupConfig>
-    ionic?: IIonic
     StatusBar?: any // ionic-plugin-statusbar
     IonicDeeplink?: any
   }
