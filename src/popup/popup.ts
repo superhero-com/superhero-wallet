4c3eeb6ba76c3d508e1a9b20f8ae8f89a3c9d770
ZGiovas <dr.vegapunk21@gmail.com>
2023-07-11 05:22:51 +0300

    refactor: delete unnecessary imports

--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -20,12 +20,6 @@ import '@ionic/vue/css/normalize.css';
 import '@ionic/vue/css/structure.css';
 import '@ionic/vue/css/typography.css';
 
-/* Optional CSS utils that can be commented out */
-import '@ionic/vue/css/padding.css';
-import '@ionic/vue/css/text-alignment.css';
-import '@ionic/vue/css/text-transformation.css';
-import '@ionic/vue/css/flex-utils.css';
-import '@ionic/vue/css/display.css';
 import '../styles/ionic.scss';
 
 registerModals();
