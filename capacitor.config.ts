70e08692b72ba03745195911fc696dc0045c98c9
martinkaintas <martinkaintas@gmail.com>
2023-07-13 14:23:18 +0300

    fix: reset wallet action

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'shw',
-  webDir: 'dist',
+  appId: 'com.superhero.cordova',
+  appName: 'Superhero',
+  webDir: 'www',
   server: {
     androidScheme: 'https',
   },
