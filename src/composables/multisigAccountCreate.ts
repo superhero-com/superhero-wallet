7735af46520bf72494732c4433c589bc3b6cf862
Cedrik Nikita <nikita.cedrik@gmail.com>
2023-07-19 16:38:11 +0400

    fix: creation of multisig with inactive account

--- src/composables/multisigAccountCreate.ts
+++ src/composables/multisigAccountCreate.ts
@@ -145,7 +145,7 @@ export function useMultisigAccountCreate({ store }: IDefaultComposableOptions) {
       }),
       {
         modal: false,
-        onAccount: payerId,
+        fromAccount: payerId,
       } as any,
     );
 
