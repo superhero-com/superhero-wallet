fdc1420f99cd408311a66a3b7c538500404f02b2
subhod-i <subhod.ibettu@gmail.com>
2023-07-19 13:16:40 +0530

    fix: fetch confirmed transactions on load

--- src/popup/components/TransactionListItem.vue
+++ src/popup/components/TransactionListItem.vue
@@ -2,7 +2,7 @@
   <ListItemWrapper
     class="transaction-item"
     :to="redirectRoute"
-    :data-cy="transaction.pending && 'pending-txs'"
+    :data-cy="currentTransaction.pending && 'pending-txs'"
   >
     <div class="body">
       <TransactionTokenRows
