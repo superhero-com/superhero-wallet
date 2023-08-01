5a9e52ab8e540cdeec969ba5f83356cf039e57cd
ZGiovas <dr.vegapunk21@gmail.com>
2023-08-01 05:57:32 +0300

    feat: enhancement to support dapp browser card

--- src/popup/pages/Dashboard.vue
+++ src/popup/pages/Dashboard.vue
@@ -13,6 +13,18 @@
       <LatestTransactionsCard />
 
       <DashboardCard
+        v-if="isAeAccount"
+        :bg-darken="true"
+        bg-position="center"
+        :title="$t('dashboard.daeppBrowserCard.title')"
+        :description="$t('dashboard.daeppBrowserCard.description')"
+        :btn-text="$t('dashboard.daeppBrowserCard.button')"
+        :background="daeppBrowserBackground"
+        :icon="GlobeIcon"
+        :to="{ name: ROUTE_APPS_BROWSER }"
+        :card-id="DASHBOARD_CARD_ID.daeppBrowser"
+      />
+      <DashboardCard
         v-if="isNodeMainnet && UNFINISHED_FEATURES"
         :title="$t('dashboard.buyCard.title')"
         :description="$t('dashboard.buyCard.description')"
@@ -39,14 +51,14 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, computed } from 'vue';
 
 import { useStore } from 'vuex';
 import {
   DASHBOARD_CARD_ID,
   PROTOCOL_AETERNITY,
 } from '@/constants';
-import { ROUTE_ACCOUNT_DETAILS_NAMES_CLAIM } from '@/popup/router/routeNames';
+import { ROUTE_ACCOUNT_DETAILS_NAMES_CLAIM, ROUTE_APPS_BROWSER } from '@/popup/router/routeNames';
 import { useAccounts, useAeSdk } from '@/composables';
 
 import DashboardCard from '../components/DashboardCard.vue';
@@ -60,9 +72,11 @@ import ArrowReceiveIcon from '../../icons/arrow-receive.svg?vue-component';
 import ArrowSendIcon from '../../icons/arrow-send.svg?vue-component';
 import CardIcon from '../../icons/credit-card.svg?vue-component';
 import MenuCardIcon from '../../icons/menu-card-fill.svg?vue-component';
+import GlobeIcon from '../../icons/globe-small.svg?vue-component';
 
 import buyBackground from '../../image/dashboard/buy-ae.jpg';
 import chainNameBackground from '../../image/dashboard/chain-name.jpg';
+import daeppBrowserBackground from '../../image/dashboard/daepp-browser.png';
 
 export default defineComponent({
   name: 'Dashboard',
@@ -84,23 +98,28 @@ export default defineComponent({
     } = useAccounts({ store });
 
     const { isNodeMainnet, isNodeTestnet } = useAeSdk({ store });
+    const isAeAccount = computed(() => activeAccount.value.protocol === PROTOCOL_AETERNITY);
 
     return {
       PROTOCOL_AETERNITY,
       DASHBOARD_CARD_ID,
       UNFINISHED_FEATURES: process.env.UNFINISHED_FEATURES,
       ROUTE_ACCOUNT_DETAILS_NAMES_CLAIM,
+      ROUTE_APPS_BROWSER,
       ArrowSendIcon,
       ArrowReceiveIcon,
       CardIcon,
       MenuCardIcon,
       activeAccount,
+      GlobeIcon,
       activeAccountSimplexLink,
       activeAccountFaucetUrl,
       buyBackground,
       chainNameBackground,
+      daeppBrowserBackground,
       isNodeMainnet,
       isNodeTestnet,
+      isAeAccount,
     };
   },
 });
