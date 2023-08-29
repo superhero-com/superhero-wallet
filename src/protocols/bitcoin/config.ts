77756dd8e544f72363ee44a0775e01bc05d17223
subhod-i <subhod.ibettu@gmail.com>
2023-08-29 15:57:27 +0530

    feat(multichain): add dust amount restriction for bitcoin transfers

--- src/protocols/bitcoin/config.ts
+++ src/protocols/bitcoin/config.ts
@@ -6,6 +6,11 @@ export const BTC_SYMBOL = 'BTC';
 export const BTC_COIN_NAME = 'Bitcoin';
 export const BTC_COIN_PRECISION = 8; // Amount of decimals
 export const BTC_COINGECKO_COIN_ID = 'bitcoin';
+/**
+ * Lower cap is 294 for segwit transactions and 546 uppercap for non-sewgit transactions
+ * https://github.com/bitcoin/bitcoin/blob/25.x/src/policy/policy.cpp#L36C1-L36C1
+ */
+export const DUST_AMOUNT = 546;
 
 export const BTC_NETWORK_DEFAULT_SETTINGS: INetworkTypeProtocolDefaultSettings = {
   [NETWORK_TYPE_MAINNET]: {
