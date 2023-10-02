878633bc9ae50728b23587db4dc240f545b1a8ef
Paweł Stachel <p.stachel@qarbon.it>
2023-10-02 18:08:36 +0200

    refactor(transaction-details): extract protocol specific components from base

--- src/popup/components/TransactionDetailsBase.vue
+++ src/popup/components/TransactionDetailsBase.vue
@@ -1,26 +1,18 @@
 <template>
   <div class="transaction-details-base">
     <div
-      v-if="!isDexAllowance || isErrorTransaction"
+      v-if="showHeader || isErrorTransaction"
       class="header"
     >
       <TransactionErrorStatus
         v-if="isErrorTransaction"
         :return-type="transaction.tx.returnType"
       />
-      <TransactionTokens
-        :ext-tokens="noneAeCoin"
-        :is-rounded="!!noneAeCoin"
-        :transaction="transaction"
-        :direction="direction"
-        :is-allowance="isDexAllowance"
-        :error="isErrorTransaction"
-        :class="{ reverse: isPool }"
-        icon-size="md"
-      />
+      <slot name="tokens" />
     </div>
     <div class="content">
       <TransactionOverview :transaction="transaction" />
+
       <div class="explorer">
         <LinkButton :to="explorerUrl">
           {{ $t('pages.transactionDetails.explorer') }}
@@ -30,10 +22,10 @@
         </LinkButton>
       </div>
       <div class="data-grid">
-        <template v-if="isSwap && !isErrorTransaction">
-          <SwapRates :transaction="transaction" />
-          <SwapRoute :transaction="transaction" />
-        </template>
+        <slot
+          v-if="!isErrorTransaction"
+          name="swap-data"
+        />
         <DetailsItem
           v-if="isErrorTransaction"
           :label="$t('pages.transactionDetails.reason')"
@@ -41,16 +33,7 @@
           class="reason"
           data-cy="reason"
         />
-        <TransactionDetailsPoolTokens
-          v-if="(isPool || isDexAllowance)"
-          :transaction="transaction"
-          :direction="direction"
-          :tx-function="transaction.tx.function"
-          :is-allowance="isDexAllowance"
-          :class="{ reverse: isPool }"
-        />
-
-        <slot name="tip-url" />
+        <slot name="additional-content" />
 
         <DetailsItem
           v-if="contractId && isContract(contractId)"
@@ -84,59 +67,7 @@
           </template>
         </DetailsItem>
 
-        <DetailsItem
-          v-if="multisigTransactionFeePaidBy"
-          :label="$t('pages.transactionDetails.feePaidBy')"
-          small
-        >
-          <div class="row payer-id">
-            <Avatar
-              :address="multisigTransactionFeePaidBy"
-              size="sm"
-            />
-            <div>
-              <DialogBox
-                v-if="isLocalAccountAddress(multisigTransactionFeePaidBy)"
-                class="dialog-box"
-                dense
-                position="bottom"
-              >
-                {{ $t('common.you') }}
-              </DialogBox>
-              <CopyText
-                hide-icon
-                :value="multisigTransactionFeePaidBy"
-                :copied-text="$t('common.addressCopied')"
-              >
-                <span class="text-address">
-                  {{ splitAddress(multisigTransactionFeePaidBy) }}
-                </span>
-              </CopyText>
-            </div>
-          </div>
-        </DetailsItem>
-
-        <DetailsItem
-          v-if="multisigContractId"
-          :label="$t('pages.transactionDetails.vaultContractId')"
-          small
-        >
-          <div class="row">
-            <Avatar
-              :address="multisigContractId"
-              size="sm"
-            />
-            <CopyText
-              hide-icon
-              :value="multisigContractId"
-              :copied-text="$t('common.addressCopied')"
-            >
-              <span class="text-address">
-                {{ splitAddress(multisigContractId) }}
-              </span>
-            </CopyText>
-          </div>
-        </DetailsItem>
+        <slot name="multisig-content" />
 
         <PayloadDetails :payload="payload" />
 
@@ -174,7 +105,7 @@
           />
         </div>
         <DetailsItem
-          v-if="!(isDex || isDexAllowance || isMultisig)"
+          v-if="!hideAmount"
           :label="$t('common.amount')"
           data-cy="amount"
         >
@@ -188,26 +119,9 @@
             />
           </template>
         </DetailsItem>
-        <DetailsItem
-          v-if="gasPrice"
-          :label="$t('pages.transactionDetails.gasPrice')"
-          data-cy="gas-price"
-        >
-          <template #value>
-            <TokenAmount
-              :amount="+gasPrice"
-              :protocol="PROTOCOL_AETERNITY"
-              :symbol="AE_SYMBOL"
-              hide-fiat
-            />
-          </template>
-        </DetailsItem>
-        <DetailsItem
-          v-if="gasUsed"
-          :value="gasUsed"
-          :label="$t('pages.transactionDetails.gasUsed')"
-          data-cy="gas"
-        />
+
+        <slot name="gas" />
+
         <DetailsItem
           v-if="transactionFee"
           :label="$t('transaction.fee')"
@@ -230,7 +144,6 @@
 <script lang="ts">
 import { defineComponent, PropType } from 'vue';
 
-import { PROTOCOL_AETERNITY } from '@/constants';
 import type { Protocol, ITransaction } from '@/types';
 import {
   formatDate,
@@ -240,19 +153,12 @@ import {
 import { isContract } from '@/protocols/aeternity/helpers';
 
 import TransactionOverview from '@/popup/components/TransactionOverview.vue';
-import SwapRoute from '@/popup/components/SwapRoute.vue';
-import SwapRates from '@/popup/components/SwapRates.vue';
 import TokenAmount from '@/popup/components/TokenAmount.vue';
 import DetailsItem from '@/popup/components/DetailsItem.vue';
 import LinkButton from '@/popup/components/LinkButton.vue';
-import TransactionTokens from '@/popup/components/TransactionTokenRows.vue';
 import CopyText from '@/popup/components/CopyText.vue';
-import TransactionDetailsPoolTokens from '@/popup/components/TransactionDetailsPoolTokens.vue';
 import PayloadDetails from '@/popup/components/PayloadDetails.vue';
 import TransactionErrorStatus from '@/popup/components/TransactionErrorStatus.vue';
-import Avatar from '@/popup/components/Avatar.vue';
-import DialogBox from '@/popup/components/DialogBox.vue';
-import { AE_SYMBOL } from '@/protocols/aeternity/config';
 
 import AnimatedPending from '@/icons/animated-pending.svg?vue-component';
 import ExternalLink from '@/icons/external-link.svg?vue-component';
@@ -261,49 +167,32 @@ export default defineComponent({
   components: {
     PayloadDetails,
     TransactionErrorStatus,
-    TransactionDetailsPoolTokens,
-    TransactionTokens,
     TransactionOverview,
     TokenAmount,
     DetailsItem,
     LinkButton,
     CopyText,
-    SwapRoute,
-    SwapRates,
     AnimatedPending,
     ExternalLink,
-    Avatar,
-    DialogBox,
   },
   props: {
     transaction: { type: Object as PropType<ITransaction | undefined>, required: true },
     coinSymbol: { type: String, required: true },
     transactionFee: { type: Number, required: true },
-    isSwap: { type: Boolean },
-    isPool: { type: Boolean },
     tokenSymbol: { type: String, required: true },
     totalAmount: { type: Number, required: true },
-    isErrorTransaction: { type: Boolean },
-    isDexAllowance: { type: Boolean },
-    isDex: { type: Boolean },
-    isTransactionAex9: { type: Boolean },
     payload: { type: String, default: '' },
-    isMultisig: { type: Boolean },
-    direction: { type: String, required: true },
     explorerUrl: { type: String, required: true },
-    isLocalAccountAddress: { type: Function, default: () => false },
-    gasPrice: { type: Number, default: 0 }, // In Ae
-    gasUsed: { type: Number, default: 0 },
     contractId: { type: String, default: '' },
-    multisigTransactionFeePaidBy: { type: String, default: '' },
-    multisigContractId: { type: String, default: '' },
     hash: { type: String, required: true },
     noneAeCoin: { type: Array, default: null },
     protocol: { type: String as PropType<Protocol>, required: true },
+    isErrorTransaction: Boolean,
+    showHeader: Boolean,
+    hideAmount: Boolean,
+    isTransactionAex9: Boolean,
   },
   setup: () => ({
-    PROTOCOL_AETERNITY,
-    AE_SYMBOL,
     formatDate,
     formatTime,
     splitAddress,
@@ -326,12 +215,6 @@ export default defineComponent({
     height: 16px;
   }
 
-  .row {
-    @include mixins.flex(flex-start, center, row);
-
-    gap: 8px;
-  }
-
   .header {
     @include mixins.flex(center, center, column);
 
@@ -340,28 +223,6 @@ export default defineComponent({
 
     @include mixins.mobile {
       width: 100%;
-    }
-
-    .transaction-token-rows {
-      &.reverse {
-        display: flex;
-        flex-direction: column-reverse;
-      }
-
-      :deep(.token-row) {
-        margin-bottom: 12px;
-        padding-inline: 16px;
-
-        .amount {
-          @extend %face-sans-18-regular;
-        }
-
-        .tokens {
-          @extend %face-sans-18-medium;
-
-          color: rgba(variables.$color-white, 0.75);
-        }
-      }
     }
   }
 
@@ -417,18 +278,6 @@ export default defineComponent({
             color: variables.$color-white;
           }
         }
-      }
-    }
-
-    .payer-id {
-      position: relative;
-
-      .dialog-box {
-        width: 30px;
-        height: 20px;
-        position: absolute;
-        right: 15px;
-        top: -28px;
       }
     }
   }
