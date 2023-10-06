33812e77945bb33622fdcab715bea726f8b67d0c
martinkaintas <martinkaintas@gmail.com>
2023-10-06 15:12:06 +0300

    feat: use capacitor plugin for network status

--- src/composables/connection.ts
+++ src/composables/connection.ts
@@ -1,14 +1,29 @@
 import { computed, ref } from 'vue';
+import { Network } from '@capacitor/network';
+import { IS_MOBILE_APP } from '@/constants';
 
 let isConnectionStatusWatched = false;
 const isOnline = ref(window.navigator.onLine);
 
 export function useConnection() {
+  if (IS_MOBILE_APP && !isConnectionStatusWatched) {
+    Network.getStatus().then((status) => {
+      isOnline.value = status.connected;
+    });
+  }
+
   function watchConnectionStatus() {
     if (!isConnectionStatusWatched) {
       isConnectionStatusWatched = true;
-      window.addEventListener('online', () => { isOnline.value = true; });
-      window.addEventListener('offline', () => { isOnline.value = false; });
+
+      if (IS_MOBILE_APP) {
+        Network.addListener('networkStatusChange', (status) => {
+          isOnline.value = status.connected;
+        });
+      } else {
+        window.addEventListener('online', () => { isOnline.value = true; });
+        window.addEventListener('offline', () => { isOnline.value = false; });
+      }
     }
   }
 
