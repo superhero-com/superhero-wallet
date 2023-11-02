c4b8cf457581ae333af97973ed26d8ead5f4a5e6
Bartosz <bartosz@peronczyk.com>
2023-11-02 14:15:57 +0100

    feat: ethereum support initial structure

---
+++ src/protocols/ethereum/config.ts
@@ -0,0 +1,5 @@
+export const ETH_SYMBOL = 'ETH';
+export const ETH_COIN_NAME = 'Ethereum';
+export const ETH_PROTOCOL_NAME = 'Ethereum';
+export const ETH_COIN_PRECISION = 18; // Amount of decimals
+export const ETH_COINGECKO_COIN_ID = 'ethereum';
