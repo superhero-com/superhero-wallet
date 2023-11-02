2dbf10fa1105028e4cea0b506530fc5f93658e57
Bartosz <bartosz@peronczyk.com>
2023-11-02 14:15:57 +0100

    feat: ethereum support initial structure

--- src/protocols/registerAdapters.ts
+++ src/protocols/registerAdapters.ts
@@ -1,7 +1,17 @@
-import { PROTOCOL_AETERNITY, PROTOCOL_BITCOIN } from '@/constants';
+import type { Class, Protocol } from '@/types';
+import { PROTOCOL_AETERNITY, PROTOCOL_BITCOIN, PROTOCOL_ETHEREUM } from '@/constants';
 import { ProtocolAdapterFactory } from '@/lib/ProtocolAdapterFactory';
 import { AeternityAdapter } from '@/protocols/aeternity/libs/AeternityAdapter';
 import { BitcoinAdapter } from '@/protocols/bitcoin/libs/BitcoinAdapter';
+import { EthereumAdapter } from '@/protocols/ethereum/libs/EthereumAdapter';
+import { BaseProtocolAdapter } from './BaseProtocolAdapter';
 
-ProtocolAdapterFactory.registerAdapter(PROTOCOL_AETERNITY, AeternityAdapter);
-ProtocolAdapterFactory.registerAdapter(PROTOCOL_BITCOIN, BitcoinAdapter);
+const protocolAdapters: Record<Protocol, Class<BaseProtocolAdapter>> = {
+  [PROTOCOL_AETERNITY]: AeternityAdapter,
+  [PROTOCOL_BITCOIN]: BitcoinAdapter,
+  [PROTOCOL_ETHEREUM]: EthereumAdapter,
+};
+
+Object.entries(protocolAdapters).forEach(([protocol, adapter]) => {
+  ProtocolAdapterFactory.registerAdapter(protocol as Protocol, adapter);
+});
