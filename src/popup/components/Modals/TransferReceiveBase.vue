0d361f415602e061d8bbd361c0d7b517ed0667d0
martinkaintas <martinkaintas@gmail.com>
2023-11-02 13:53:14 +0200

    feat: add eth transfer receive view

--- src/popup/components/Modals/TransferReceiveBase.vue
+++ src/popup/components/Modals/TransferReceiveBase.vue
@@ -41,7 +41,7 @@
           <Scrollable class="address-scrollable-area">
             <AddressFormatted
               :address="accountAddressToDisplay"
-              :split-address="protocol === PROTOCOL_BITCOIN && !amount"
+              :split-address="splitAddress"
             />
           </Scrollable>
         </CopyText>
@@ -111,6 +111,7 @@ import type {
 import {
   IS_MOBILE_DEVICE,
   PROTOCOL_BITCOIN,
+  PROTOCOL_ETHEREUM,
 } from '@/constants';
 import { RouteQueryActionsController } from '@/lib/RouteQueryActionsController';
 import { useAccounts, useCopy } from '@/composables';
@@ -131,6 +132,9 @@ import CopyText from '../CopyText.vue';
 import AccountItem from '../AccountItem.vue';
 
 import ShareIcon from '../../../icons/share.svg?vue-component';
+
+// TODO - validate all addresses instead of 'splitAddress' prop, see AddressFormatted.vue
+const SPLIT_PROTOCOL_ADDRESS = [PROTOCOL_BITCOIN, PROTOCOL_ETHEREUM];
 
 export default defineComponent({
   name: 'TransferReceiveBase',
@@ -180,6 +184,10 @@ export default defineComponent({
         ? RouteQueryActionsController.createUrl('/account', 'transferSend', getTokenInfoQuery(address))
         : '';
     }
+
+    const splitAddress = computed(
+      () => SPLIT_PROTOCOL_ADDRESS.includes(props.protocol) && !amount.value,
+    );
 
     const accountAddressToCopy = computed(
       () => (amount.value && +amount.value > 0)
@@ -239,10 +247,12 @@ export default defineComponent({
 
     return {
       PROTOCOL_BITCOIN,
+      PROTOCOL_ETHEREUM,
       IS_MOBILE_DEVICE,
       ShareIcon,
       amount,
       selectedAsset,
+      splitAddress,
       share,
       handleAssetChange,
       copyAddress,
---
+++ src/protocols/ethereum/views/TransferReceiveModal.vue
@@ -0,0 +1,36 @@
+<template>
+  <TransferReceiveBase
+    :heading="$t('modals.receive.title', { name: protocolName })"
+    :account-address="activeAccount.address"
+    :protocol="PROTOCOL_ETHEREUM"
+    disable-asset-selection
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import {
+  PROTOCOL_ETHEREUM,
+  PROTOCOL_VIEW_TRANSFER_RECEIVE,
+} from '@/constants';
+import { useAccounts } from '@/composables';
+import { ProtocolAdapterFactory } from '@/lib/ProtocolAdapterFactory';
+
+import TransferReceiveBase from '@/popup/components/Modals/TransferReceiveBase.vue';
+
+export default defineComponent({
+  name: PROTOCOL_VIEW_TRANSFER_RECEIVE,
+  components: {
+    TransferReceiveBase,
+  },
+  setup() {
+    const { activeAccount } = useAccounts();
+
+    return {
+      PROTOCOL_ETHEREUM,
+      activeAccount,
+      protocolName: ProtocolAdapterFactory.getAdapter(PROTOCOL_ETHEREUM).protocolName,
+    };
+  },
+});
+</script>
