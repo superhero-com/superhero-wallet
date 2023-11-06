8ff5a3b0a6bf86598d7abb6e7539a5cb4ac45baa
martinkaintas <martinkaintas@gmail.com>
2023-11-06 12:07:42 +0200

    feat: calculate ethereum fees

--- src/protocols/ethereum/helpers/index.ts
+++ src/protocols/ethereum/helpers/index.ts
@@ -1,5 +1,14 @@
 import { isAddress } from 'web3-validator';
+import { fromWei, toWei } from 'web3-utils';
+import BigNumber from 'bignumber.js';
 
 export function isEthAddressValid(address: string) {
   return isAddress(address);
+}
+
+/**
+ * Convert Gwei to Ether
+ */
+export function etherFromGwei(gwei: number) {
+  return new BigNumber(fromWei(toWei(gwei, 'gwei'), 'ether'));
 }
