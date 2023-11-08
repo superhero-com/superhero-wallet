f6de81182226b2c255ac1a0fa85ae7d3532ffdfd
Bartosz <bartosz@peronczyk.com>
2023-11-08 09:51:18 +0100

    feat: ethereum account details page

---
+++ src/protocols/ethereum/views/AccountDetails.vue
@@ -0,0 +1,32 @@
+<template>
+  <IonPage>
+    <IonContent class="account-ion-content">
+      <AccountDetailsBase class="account-details" />
+    </IonContent>
+  </IonPage>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { IonContent, IonPage } from '@ionic/vue';
+import { PROTOCOL_VIEW_ACCOUNT_DETAILS } from '@/constants';
+import AccountDetailsBase from '@/popup/components/AccountDetailsBase.vue';
+
+export default defineComponent({
+  name: PROTOCOL_VIEW_ACCOUNT_DETAILS,
+  components: {
+    AccountDetailsBase,
+    IonPage,
+    IonContent,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+@use '@/styles/variables';
+
+.account-ion-content {
+  overflow: hidden;
+  background-color: variables.$color-bg-4;
+}
+</style>
