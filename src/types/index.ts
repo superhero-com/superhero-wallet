718a5bbda92fc9cd4cda6b99f012223900e5273e
martinkaintas <martinkaintas@gmail.com>
2023-11-09 15:47:38 +0200

    feat: eth spend

--- src/types/index.ts
+++ src/types/index.ts
@@ -94,11 +94,6 @@ export interface IPageableResponse<T> {
   next: string;
 }
 
-export type IKeyPair = {
-  publicKey: Uint8Array;
-  secretKey: Uint8Array;
-};
-
 export interface IAppData {
   name: string;
   url: string;
@@ -702,6 +697,8 @@ export interface TransferFormModel extends IFormModel {
   invoiceContract?: any;
   invoiceId?: any;
   note?: string;
+  maxPriorityFeePerGas?: string;
+  maxFeePerGas?: string;
   payload: string;
 }
 
