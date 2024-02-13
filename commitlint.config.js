5eb7a94927e0ec2dd207a3d6b467ae46b3aa2f8f
Nikita <nikita.cedrik@gmail.com>
2024-02-13 16:55:12 +0400

    build: update commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,28 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-case': [
+      2,
+      'always',
+      ['sentence-case', 'start-case', 'pascal-case', 'upper-case', 'lower-case'],
+    ],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'test',
+        'sample',
+      ],
+    ],
+  },
 };
