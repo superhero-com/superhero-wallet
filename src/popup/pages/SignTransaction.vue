1e4aae3ee9e88f31cea456b09d916b9522c2aced
Philipp <github@piwo.space>
2024-03-26 10:10:57 +0100

    feat: allow to replace caller in sign transaction

--- src/popup/pages/SignTransaction.vue
+++ src/popup/pages/SignTransaction.vue
@@ -8,9 +8,11 @@ import { IonPage } from '@ionic/vue';
 import { defineComponent, onMounted } from 'vue';
 import { useRoute } from 'vue-router';
 import { useI18n } from 'vue-i18n';
-import { Encoded } from '@aeternity/aepp-sdk';
+import {
+  buildTx, Encoded, Tag, unpackTx,
+} from '@aeternity/aepp-sdk';
 
-import { DEFAULT_WAITING_HEIGHT } from '@/constants';
+import { DEFAULT_WAITING_HEIGHT, PROTOCOLS } from '@/constants';
 import { RejectedByUserError } from '@/lib/errors';
 import { handleUnknownError } from '@/utils';
 import {
@@ -18,6 +20,7 @@ import {
   useModals,
   useAeSdk,
   useUi,
+  useAccounts,
 } from '@/composables';
 
 export default defineComponent({
@@ -34,11 +37,30 @@ export default defineComponent({
       const { nodeNetworkId, getAeSdk } = useAeSdk();
       const { openDefaultModal } = useModals();
       const { setLoaderVisible } = useUi();
+      const { getLastActiveProtocolAccount } = useAccounts();
+
+      const aeSdk = await getAeSdk();
+      const activeAccountAddress = getLastActiveProtocolAccount(
+        PROTOCOLS.aeternity,
+      )?.address as Encoded.AccountAddress;
+
+      async function replaceCallerTx(tx: Encoded.Transaction, callerId: Encoded.AccountAddress) {
+        const unpackedTx = unpackTx(tx) as ReturnType<typeof unpackTx>
+          & { callerId: Encoded.AccountAddress };
+
+        if (unpackedTx.tag === Tag.ContractCallTx || unpackedTx.tag === Tag.ContractCreateTx) {
+          unpackedTx.callerId = callerId;
+          unpackedTx.nonce = (await aeSdk.api.getAccountByPubkey(callerId)).nonce + 1;
+        }
+
+        return buildTx(unpackedTx);
+      }
 
       try {
         setLoaderVisible(true);
-        const aeSdk = await getAeSdk();
-        const { transaction, networkId, broadcast } = route.query;
+        const {
+          transaction, networkId, broadcast, 'replace-caller': replaceCaller,
+        } = route.query;
 
         if (networkId !== nodeNetworkId.value) {
           await openDefaultModal({
@@ -51,8 +73,16 @@ export default defineComponent({
           return;
         }
 
+        // replacing the caller might be chosen by an aepp if no wallet connection is available
+        // but a transaction should be proposed to the user anyway
+        // e.g. to reduce the number of steps necessary in a deep-link environment,
+        // from two round trips, aepp -> wallet -> aepp, to one
+        const txToSign = replaceCaller === 'true'
+          ? await replaceCallerTx(transaction as Encoded.Transaction, activeAccountAddress)
+          : transaction;
+
         const signedTransaction = await aeSdk.signTransaction(
-          decodeURIComponent(transaction as string) as Encoded.Transaction,
+          decodeURIComponent(txToSign as string) as Encoded.Transaction,
           {
             networkId,
             aeppOrigin: callbackOrigin.value?.toString(),
