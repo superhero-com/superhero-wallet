8fb7e71b7b967ffc43d368f54b00b0d3e7175b28
Bartosz <bartosz@peronczyk.com>
2024-03-27 12:06:08 +0100

    fix: seed phrase notification position

---
+++ src/popup/components/CardMnemonic.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="card-mnemonic">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'CardMnemonic',
+});
+</script>
+
+<style lang="scss" scoped>
+@use '../../styles/variables' as *;
+
+.card-mnemonic {
+  background: rgba($color-white, 0.15);
+  border: 2px solid rgba($color-white, 0.1);
+  border-radius: $border-radius-modal;
+  padding: 12px;
+}
+</style>
--- src/popup/pages/SeedPhraseVerifySettings.vue
+++ src/popup/pages/SeedPhraseVerifySettings.vue
@@ -3,13 +3,15 @@
     <IonContent class="ion-padding ion-content-bg">
       <div class="seed-phrase-verify-settings">
         <div class="seed-phrase-verify-settings-body">
-          <div class="text-heading-1">
-            {{ $t('pages.seed-phrase-settings.verifyYourSeedPhrase') }}
-          </div>
+          <div
+            class="text-heading-1"
+            v-text="$t('pages.seed-phrase-settings.verifyYourSeedPhrase')"
+          />
 
-          <div class="text-description">
-            {{ $t('pages.seed-phrase-settings.confirm-that-you-save-your-seed-phrase') }}
-          </div>
+          <div
+            class="text-description"
+            v-text="$t('pages.seed-phrase-settings.confirm-that-you-save-your-seed-phrase')"
+          />
           <i18n-t
             keypath="pages.seed-phrase-settings.compose-your-seed-phrase"
             tag="div"
@@ -21,7 +23,7 @@
             </strong>
           </i18n-t>
 
-          <div class="phraser">
+          <div class="source-phrases">
             <SeedPhraseBadge
               v-for="(word, index) in mnemonicShuffled"
               :key="index"
@@ -31,30 +33,35 @@
             />
           </div>
 
-          <div
-            ref="phraserLightEl"
-            class="phraser bright"
-          >
-            <template v-if="!selectedWordIds.length">
-              <SeedPhraseBadge
-                v-for="(word, index) in examplePhrase"
-                :key="index"
-                :text="word"
-                selected
-                editable
-              />
-            </template>
-            <template v-else>
-              <SeedPhraseBadge
-                v-for="(id, index) in selectedWordIds"
-                :key="id"
-                :text="mnemonicShuffled[id]"
-                editable
-                @click="selectedWordIds.splice(index, 1)"
-              />
-            </template>
+          <div class="selected-phrases">
+            <SeedPhraseNotification
+              v-if="showNotification"
+              class="selected-phrases-notification"
+              :has-error="hasError"
+            />
+            <CardMnemonic class="selected-phrases-list">
+              <template v-if="!selectedWordIds.length">
+                <SeedPhraseBadge
+                  v-for="(word, index) in examplePhrase"
+                  :key="index"
+                  :text="word"
+                  selected
+                  editable
+                />
+              </template>
+              <template v-else>
+                <SeedPhraseBadge
+                  v-for="(id, index) in selectedWordIds"
+                  :key="id"
+                  :text="mnemonicShuffled[id]"
+                  editable
+                  @click="selectedWordIds.splice(index, 1)"
+                />
+              </template>
+            </CardMnemonic>
           </div>
         </div>
+
         <FixedScreenFooter>
           <BtnMain
             v-if="hasError || !showNotification"
@@ -68,11 +75,6 @@
             :text="$t('common.backToHome')"
             :to="{ name: ROUTE_ACCOUNT }"
           />
-          <SeedPhraseNotification
-            v-if="showNotification"
-            :has-error="hasError"
-            :phraser-el="phraserLightEl"
-          />
         </FixedScreenFooter>
       </div>
     </IonContent>
@@ -80,18 +82,16 @@
 </template>
 
 <script lang="ts">
-import {
-  defineComponent,
-  ref,
-  computed,
-} from 'vue';
+import { defineComponent, ref, computed } from 'vue';
 import { shuffle } from 'lodash-es';
 import { useI18n } from 'vue-i18n';
 import { IonPage, IonContent, onIonViewWillLeave } from '@ionic/vue';
+
 import { useAccounts, useNotifications, useUi } from '@/composables';
 import { ROUTE_ACCOUNT } from '@/popup/router/routeNames';
 
 import BtnMain from '../components/buttons/BtnMain.vue';
+import CardMnemonic from '../components/CardMnemonic.vue';
 import FixedScreenFooter from '../components/FixedScreenFooter.vue';
 import SeedPhraseNotification from '../components/SeedPhraseNotification.vue';
 import SeedPhraseBadge from '../components/SeedPhraseBadge.vue';
@@ -102,12 +102,11 @@ export default defineComponent({
     SeedPhraseNotification,
     FixedScreenFooter,
     BtnMain,
+    CardMnemonic,
     IonPage,
     IonContent,
   },
   setup() {
-    const phraserLightEl = ref<HTMLElement | undefined>(undefined);
-
     const { t } = useI18n();
 
     const { setBackedUpSeed } = useUi();
@@ -115,8 +114,8 @@ export default defineComponent({
     const { removeIsSeedBackedUpNotification } = useNotifications({ requirePolling: false });
 
     const selectedWordIds = ref<number[]>([]);
-    const showNotification = ref<boolean>(false);
-    const hasError = ref<boolean>(false);
+    const showNotification = ref(false);
+    const hasError = ref(false);
     const examplePhrase = ref([t('pages.seedPhrase.first'), t('pages.seedPhrase.second'), '...']);
 
     const mnemonicShuffled = computed((): string[] => shuffle(mnemonic.value.split(' ')));
@@ -132,6 +131,7 @@ export default defineComponent({
         .join(' ');
       showNotification.value = true;
       hasError.value = mnemonic.value !== mnemonicSelected;
+
       if (!hasError.value) {
         setBackedUpSeed(true);
         removeIsSeedBackedUpNotification();
@@ -156,7 +156,6 @@ export default defineComponent({
 
     return {
       ROUTE_ACCOUNT,
-      phraserLightEl,
       selectedWordIds,
       showNotification,
       hasError,
@@ -176,28 +175,36 @@ export default defineComponent({
 @use '../../styles/typography';
 
 .seed-phrase-verify-settings {
+  position: relative;
+
   &-body {
     padding: var(--screen-padding-x);
   }
 
   .title {
+    @extend %face-sans-18-regular;
+
     color: rgba(variables.$color-white, 1);
     margin-bottom: 18px;
     text-align: center;
-
-    @extend %face-sans-18-regular;
   }
 
-  .phraser {
-    padding: 0;
-    margin: 18px 0 0 0;
+  .source-phrases {
+    margin-top: 18px;
+  }
 
-    &.bright {
-      background: rgba(variables.$color-white, 0.15);
-      border: 2px solid rgba(variables.$color-white, 0.1);
-      border-radius: variables.$border-radius-card;
-      padding: 12px 8px;
-      min-height: 176px;
+  .selected-phrases {
+    position: relative;
+    margin-top: 18px;
+
+    .selected-phrases-list {
+      min-height: 180px; // 4 rows of badges
+    }
+
+    .selected-phrases-notification {
+      position: absolute;
+      z-index: 1;
+      inset: 0;
     }
   }
 }
