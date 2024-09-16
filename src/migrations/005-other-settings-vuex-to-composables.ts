749618dd2a11ebd5ba39b5d085a21fc1ab8d48ab
Martin Kaintas <martinkaintas@gmail.com>
2024-09-16 16:06:49 +0300

    refactor: translate password strength value & simplify AUTHENTICATION_TIMEOUTS const

--- src/migrations/005-other-settings-vuex-to-composables.ts
+++ src/migrations/005-other-settings-vuex-to-composables.ts
@@ -1,5 +1,5 @@
 import type { Migration, IOtherSettings } from '@/types';
-import { AUTHENTICATION_TIMEOUTS, IS_MOBILE_APP } from '@/constants';
+import { AUTHENTICATION_TIMEOUT_DEFAULT } from '@/constants';
 import { collectVuexState } from './migrationHelpers';
 
 const migration: Migration<IOtherSettings> = async (restoredValue: IOtherSettings) => {
@@ -8,7 +8,7 @@ const migration: Migration<IOtherSettings> = async (restoredValue: IOtherSetting
     return {
       isSeedBackedUp: backedUpSeed,
       saveErrorLog,
-      secureLoginTimeout: IS_MOBILE_APP ? AUTHENTICATION_TIMEOUTS[0] : AUTHENTICATION_TIMEOUTS[5],
+      secureLoginTimeout: AUTHENTICATION_TIMEOUT_DEFAULT,
     };
   }
   return restoredValue;
