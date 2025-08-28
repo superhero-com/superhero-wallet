2fdb6b79f3c8b4ecde7bc0014ba98a989aca2093
akalogerakis <akalogerakis@unicorndomain.gr>
2025-08-28 17:36:57 +0300

    feat: supericon for evm in address book

--- src/popup/components/ProtocolIcon.vue
+++ src/popup/components/ProtocolIcon.vue
@@ -1,7 +1,38 @@
 <template>
+  <!-- For EVM protocols, show multiple coin icons only when showSuperIcon is true -->
+  <div
+    v-if="isEvmProtocol && showSuperIcon"
+    class="evm-protocol-icons"
+    :class="[iconSize]"
+  >
+    <component
+      v-for="(icon, index) in evmIcons.slice(0, maxVisibleIcons)"
+      :key="index"
+      :is="icon"
+      class="protocol-icon"
+      :class="[iconSize]"
+      :style="{
+        marginLeft: index > 0 ? '-10px' : '0',
+        zIndex: maxVisibleIcons + 1 - index,
+      }"
+    />
+    <span
+      v-if="evmIcons.length > maxVisibleIcons"
+      class="icon protocol-icon evm-more-indicator"
+      :class="[iconSize]"
+      :style="{
+        marginLeft: '-10px',
+        zIndex: 0,
+      }"
+    >
+      &middot;&middot;&middot;
+    </span>
+  </div>
+
+  <!-- For non-EVM protocols or when showSuperIcon is false, show single icon -->
   <component
+    v-else-if="selectedIcon"
     :is="selectedIcon"
-    v-if="selectedIcon"
     class="protocol-icon"
     :class="[iconSize]"
   />
@@ -14,12 +45,12 @@ import {
   defineComponent,
   PropType,
 } from 'vue';
-import { ICON_SIZES, PROTOCOLS } from '@/constants';
+import { ICON_SIZES, PROTOCOLS, EVM_PROTOCOLS } from '@/constants';
 import type { Protocol } from '@/types';
+import { isEvm } from '@/utils';
 import AeternityLogo from '@/icons/logo/aeternity.svg?vue-component';
-import BitcoinIcon from '@/icons/coin/bitcoin.svg?vue-component';
-import EthereumIcon from '@/icons/coin/ethereum.svg?vue-component';
-import BnbIcon from '@/icons/coin/bnb.svg?vue-component';
+
+import { COIN_ICONS } from './AssetIcon.vue';
 
 const SIZES = [
   ICON_SIZES.xs,
@@ -42,19 +73,33 @@ export default defineComponent({
       default: ICON_SIZES.md,
       validator: (val: AllowedProtocolIconSize) => SIZES.includes(val),
     },
+    showSuperIcon: {
+      type: Boolean,
+      default: false,
+    },
+    maxVisibleIcons: {
+      type: Number,
+      default: 3,
+      validator: (val: number) => val > 0,
+    },
   },
   setup(props) {
     const iconsMap: Record<Protocol, Component> = {
-      [PROTOCOLS.aeternity]: AeternityLogo,
-      [PROTOCOLS.bitcoin]: BitcoinIcon,
-      [PROTOCOLS.ethereum]: EthereumIcon,
-      [PROTOCOLS.bnb]: BnbIcon,
+      ...COIN_ICONS,
+      [PROTOCOLS.aeternity]: AeternityLogo, // Override with logo version
     };
 
     const selectedIcon = computed((): Component => iconsMap[props.protocol]);
 
+    const isEvmProtocol = computed((): boolean => isEvm(props.protocol));
+    const evmIcons = computed((): Component[] => (
+      EVM_PROTOCOLS.map((protocol) => iconsMap[protocol])
+    ));
+
     return {
       selectedIcon,
+      isEvmProtocol,
+      evmIcons: evmIcons.value.concat(evmIcons.value),
     };
   },
 });
@@ -82,6 +127,29 @@ export default defineComponent({
 
   &.xl {
     --icon-size: var(--icon-size-xl);
+  }
+}
+
+.evm-protocol-icons {
+  display: inline-flex;
+  align-items: center;
+
+  .protocol-icon {
+    width: var(--icon-size-sm) !important;
+    height: var(--icon-size-sm) !important;
+  }
+
+  .evm-more-indicator {
+    font-weight: bold;
+    color: #a3a3a3;
+    background-color: #464646;
+    border-radius: 50%;
+    font-size: 12px;
+    width: var(--icon-size-sm) !important;
+    height: var(--icon-size-sm) !important;
+    display: flex;
+    justify-content: center;
+    align-items: center;
   }
 }
 </style>
