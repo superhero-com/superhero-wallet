4f5b085927ef03275f8efec626d95ee4651f354b
akalogerakis <akalogerakis@unicorndomain.gr>
2025-08-29 04:06:40 +0300

    feat: avalanche c-chain support

--- src/popup/components/AssetIcon.vue
+++ src/popup/components/AssetIcon.vue
@@ -46,6 +46,7 @@ import BitcoinIcon from '@/icons/coin/bitcoin.svg?vue-component';
 import EthereumIcon from '@/icons/coin/ethereum.svg?vue-component';
 import SolanaIcon from '@/icons/coin/solana.svg?vue-component';
 import BnbIcon from '@/icons/coin/bnb.svg?vue-component';
+import AvalancheIcon from '@/icons/coin/avalanche.svg?vue-component';
 import LexonTokenIcon from '@/icons/tokens/ct_xtk8rSz9suPb6D6VLquyfVji25FcnFRDjn3dnn5mmvHsPiESt.svg?vue-component';
 
 const SIZES = [ICON_SIZES.sm, ICON_SIZES.rg, ICON_SIZES.md, ICON_SIZES.lg, ICON_SIZES.xxl] as const;
@@ -58,6 +59,7 @@ export const COIN_ICONS: Record<Protocol, Component> = {
   [PROTOCOLS.ethereum]: EthereumIcon,
   [PROTOCOLS.solana]: SolanaIcon,
   [PROTOCOLS.bnb]: BnbIcon,
+  [PROTOCOLS.avalanche]: AvalancheIcon,
 };
 
 const ASSET_ICONS: Dictionary<Component> = {
